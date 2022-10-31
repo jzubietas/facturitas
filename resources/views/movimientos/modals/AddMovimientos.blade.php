ad7113b59e51a18b3a1a977c07137ac6813b49f6
rvalverderuiz28 <rvalverderuiz28@gmail.com>
2022-10-31 09:41:23 -0500

    cambios 311022-00

--- resources/views/movimientos/modals/AddMovimientos.blade.php
+++ resources/views/movimientos/modals/AddMovimientos.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header bg-primary">
-          <h5 class="modal-title" id="exampleModalLabel">Agregar Pagos</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Agregar movimientos</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
