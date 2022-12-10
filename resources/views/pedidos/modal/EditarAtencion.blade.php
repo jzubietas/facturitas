946290a7e5794f750c8814a96a2e81ad969b5c7e
TeoEstudio <dei.guiribalde@gmail.com>
2022-12-10 13:22:10 -0500

    Eliminar Adjunto de pedido V1

---
+++ resources/views/pedidos/modal/EditarAtencion.blade.php
@@ -0,0 +1,44 @@
+  <!-- Modal -->
+  <div class="modal fade" id="modal-editar-atencion" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" style="max-width: 800px!important;">
+      <div class="modal-content">
+        <div class="modal-header bg-success">
+          <h5 class="modal-title" id="exampleModalLabel">Editar Atención</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        {{-- Form::Open(['route' => ['pedidos.atender', $pedido],'enctype'=>'multipart/form-data', 'id'=>'formulario','files'=>true]) --}}
+        <form id="formularioatender" name="formularioatender" enctype="multipart/form-data">
+          <input type="hidden" id="hiddenAtender" name="hiddenAtender">
+        <div class="modal-body">
+          <p>Detalles del pedido: <strong class="textcode">PED00</strong></p>
+        </div>
+        <div style="margin: 10px">
+          <div class="card">
+            <div class="border rounded card-body border-secondary">
+              <div class="card-body">
+                <div class="form-row">
+                  <div class="form-group col-lg-12">
+                    <div class="row">
+                      <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                        <h5><b>Archivos adjuntos:</b></h5>
+                      </div>
+                      <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12" id="listado_adjuntos">
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+          <button type="submit" class="btn btn-info" id="atender">Confirmar</button>
+        </div>
+        {{ Form::Close() }}
+      </div>
+    </div>
+  </div>
