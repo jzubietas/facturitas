620b0e0b8c3b7e001621cbce6356fa60a889d639
LuisMl <iluismejialeon@gmail.com>
2022-12-11 23:51:01 -0500

    fixed

--- resources/views/dashboard/searchs/search_cliente.blade.php
+++ resources/views/dashboard/searchs/search_cliente.blade.php
@@ -3,52 +3,37 @@
 
         <div class="col-md-6">
             <div class="card mt-4" style=" background: rgba(25,255,247,0.06); ">
-                <div class="card-header">
-                    <strong>Datos del Cliente</strong>
-                </div>
-
                 <div class="card-body">
                     <div>
                         <ul class="list-group">
-                            <li class="list-group-item">Nombre Cliente: <b>{{$cliente->nombre}}</b></li>
-                            <li class="list-group-item">DNI: <b>{{$cliente->dni}}</b></li>
-                            <li class="list-group-item">Celular: <b>{{$cliente->celular}}</b>
-                            </li>
+                            <li class="list-group-item bg-success">Cliente: <b>{{$cliente->nombre}}</b></li>
+                            <li class="list-group-item bg-success">DNI: <b>{{$cliente->dni}}</b></li>
+                            <li class="list-group-item bg-success">Celular: <b>{{$cliente->celular}}-{{$cliente->icelular}}</b></li>
+                            <li class="list-group-item bg-success">Situacion: <b>{{$cliente->situacion}}</b></li>
+                            <li class="list-group-item bg-success">Tiene Deuda: <b> No</b></li>
+                            <li class="list-group-item bg-success">Deuda Total: <b>S/0.00</b></li>
+                            @if($cliente->user!=null)
+                                <li class="list-group-item bg-info">Asesor:
+                                    <b>{{$cliente->user->identificador}}</b></li>
+                                <li class="list-group-item bg-info">Asesor Email: <b>{{$cliente->user->email}}</b></li>
+                            @else
+                                <li class="list-group-item bg-info">
+                                    <div class="alert alert-warning " role="alert">
+                                        No hay asesor asignado
+                                    </div>
+                                </li>
+                            @endif
+                            @foreach($cliente->rucs as $ruc)
+                                <li class="list-group-item bg-dark">
+                                    RUC:<b>{{$ruc->num_ruc}}</b>
+                                </li>
+                                <li class="list-group-item bg-dark border-bottom">Razon social: <b>{{$ruc->empresa??"--"}}</b></li>
+                            @endforeach
+
                         </ul>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-6">
-            <div class="card mt-4" style=" background: #ffd81914; ">
-                <div class="card-header">
-                    <strong>Datos del Agente</strong>
-                </div>
-                <div class="card-body">
-                    <div>
-                        @if($cliente->user!=null)
-                            <ul class="list-group">
-                                <li class="list-group-item bg-success">Identificador:
-                                    <b>{{$cliente->user->identificador}}</b></li>
-                                <li class="list-group-item">Asesor: <b>{{$cliente->user->name}}</b></li>
-                                <li class="list-group-item">Email: <b>{{$cliente->user->email}}</b></li>
-                                <li class="list-group-item">Celular: <b>{{$cliente->user->celular}}</b>
-                                </li>
-                                <li class="list-group-item">Referencia:
-                                    <b>{{$cliente->user->referencia}}</b>
-                                </li>
-                            </ul>
-                        @else
-                            <div class="alert alert-warning" role="alert">
-                                No hay asesor asignado
-                            </div>
-                        @endif
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-12">
-            <hr>
         </div>
     @endforeach
 
