61a2d7a3db1c2ec1f19eab03d39ac74169d0098c
TeoEstudio <dei.guiribalde@gmail.com>
2022-12-12 12:17:58 -0500

    Adjuntar mas archivos en Modal Atender Atención
    Ya se pueden adjuntar mas archivos en el Modal Editar Atención, revisar.

--- resources/views/pedidos/modal/EditarAtencion.blade.php
+++ resources/views/pedidos/modal/EditarAtencion.blade.php
@@ -9,7 +9,7 @@
           </button>
         </div>
         {{-- Form::Open(['route' => ['pedidos.atender', $pedido],'enctype'=>'multipart/form-data', 'id'=>'formulario','files'=>true]) --}}
-        <form id="formularioatender" name="formularioatender" enctype="multipart/form-data">
+
           <input type="hidden" id="hiddenAtender" name="hiddenAtender">
         <div class="modal-body">
           <p>Detalles del pedido: <strong class="textcode">PED00</strong></p>
@@ -20,14 +20,42 @@
               <div class="card-body">
                 <div class="form-row">
                   <div class="form-group col-lg-12">
-                    <div class="row">
+
+
+                      <div class="col-lg-12 col-md-6 col-sm-6 col-xs-6">
+                        <form method="POST" action="{{ route('operaciones.updateatender', 1) }}" accept-charset="UTF-8" enctype="multipart/form-data" id="formulario_adjuntos">
+                          <input name="_token" type="hidden" value="gC4bbgB44XSNjYzh2N0OS6BLkmzepKe0jRLoSqPR">
+                          <div class="row">
+                          <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+                              <label for="envio_doc">Documento enviado</label>
+                              <input type="hidden" name="_token" value="gC4bbgB44XSNjYzh2N0OS6BLkmzepKe0jRLoSqPR">
+                              <input class="form-control-file" id="adjunto" multiple="" name="adjunto[]" type="file">
+                          </div>
+                          <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+                              <label for="fecha_envio_doc">Fecha de envío</label>
+                              <input class="form-control" id="fecha_envio_doc" disabled="" name="fecha_envio_doc" type="text" value="">
+                          </div>
+
+                          <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12" style="margin-top:12px;">
+                            <button type="submit" class="btn btn-success" id="atender">Confirmar</button>
+                        </div>
+                          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                              <label for="cant_compro">Cantidad de comprobantes enviados</label>
+                              <input class="form-control" id="cant_compro" step="1" min="0" name="cant_compro" type="number" value="0" disabled>
+                          </div>
+
+                      </div>
+                        </form>
+                        <hr>
+                      </div>
+
                       <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
                         <h5><b>Archivos adjuntos:</b></h5>
                       </div>
                       <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12" id="listado_adjuntos">
 
                       </div>
-                    </div>
+
                   </div>
                 </div>
               </div>
@@ -35,24 +63,6 @@
           </div>
         </div>
         <div class="container">
-          <div class="row">
-            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-6" style="padding: 24px;">
-                {!! Form::label('envio_doc', 'Documento enviado') !!}
-                @csrf
-                {!! Form::file('adjunto[]', ['class' => 'form-control-file', 'id'=>'adjunto', 'multiple']) !!}
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-6" style="padding: 24px;">
-                {!! Form::label('fecha_envio_doc', 'Fecha de envío') !!}
-                {!! Form::text('fecha_envio_doc', '11-12-2022', ['class' => 'form-control', 'id' => 'fecha_envio_doc', 'disabled']) !!}
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-6" style="padding: 24px;">
-                {!! Form::label('cant_compro', 'Cantidad de comprobantes enviados') !!}
-                {!! Form::number('cant_compro', '1', ['class' => 'form-control', 'id' => 'cant_compro', 'step'=>'1', 'min' => '0']) !!}
-            </div>
-            <div class="col-lg-12 col-md-6 col-sm-6 col-xs-6 text-center" style="padding: 24px;">
-              <button type="submit" class="btn btn-info" id="atender">Confirmar</button>
-          </div>
-          </div>
         </div>
 
 
@@ -61,7 +71,7 @@
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
           <button type="submit" class="btn btn-info" id="atender">Confirmar</button>
         </div>
-        {{ Form::Close() }}
+
       </div>
     </div>
   </div>
