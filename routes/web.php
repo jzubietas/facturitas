86e14e4fc8c53226d532c9b35930d65c23e5248d
Mramos <mramos@widecom.net>
2022-12-14 14:57:42 -0500

    moviendo route y funciones al controller envio (operaciones)

--- routes/web.php
+++ routes/web.php
@@ -217,9 +217,9 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::post('operaciones.updateatender/{pedido}', [OperacionController::class, 'updateAtender'])->name('operaciones.updateatender');
     Route::post('operaciones.updateatenderid/{pedido}', [OperacionController::class, 'updateAtenderId'])->name('operaciones.updateatenderid');
     Route::get('operaciones.showatender/{pedido}', [OperacionController::class, 'showAtender'])->name('operaciones.showatender');
-    Route::post('operaciones.envioid', [OperacionController::class, 'Enviarid'])->name('operaciones.envioid');
+    Route::post('operaciones.envioid', [EnvioController::class, 'Enviarid'])->name('operaciones.envioid');
     Route::post('operaciones.revertirenvioid', [OperacionController::class, 'Revertirenvio'])->name('operaciones.revertirenvioid');
-    Route::post('operaciones.sinenvioid', [OperacionController::class, 'SinEnviarid'])->name('operaciones.sinenvioid');
+        Route::post('operaciones.sinenvioid', [EnvioController::class, 'SinEnviarid'])->name('operaciones.sinenvioid');
 
     /*Operaciones*/
 
