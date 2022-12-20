3f820df4f70b472cef5914f7a22e2eb755cfea46
jzubietas <jhonathanisaizubietasantos@gmail.com>
2022-12-20 15:18:46 -0500

    a

--- resources/views/sobres/modal/direccionid.blade.php
+++ resources/views/sobres/modal/direccionid.blade.php
@@ -144,12 +144,12 @@
 
                   <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     {!! Form::label('tracking', 'Tracking') !!}
-                    {!! Form::text('tracking', null, ['class' => 'form-control', 'placeholder' => 'Nombre', 'required' => 'required']) !!}
+                    {!! Form::text('tracking', null, ['class' => 'form-control', 'placeholder' => 'Nombre', 'required' => 'required','maxlength' => 12]) !!}
                   </div>
 
                   <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     {!! Form::label('numregistro', 'Numero de Registro') !!}
-                    {!! Form::text('numregistro', null, ['class' => 'form-control', 'placeholder' => 'Nombre', 'required' => 'required']) !!}
+                    {!! Form::text('numregistro', null, ['class' => 'form-control', 'placeholder' => 'Nombre', 'required' => 'required','maxlength' => 12]) !!}
                   </div>
 
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
