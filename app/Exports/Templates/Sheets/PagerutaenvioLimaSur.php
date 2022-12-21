70d1be531b4a29716f2fd2ea58d1f7b2d0ab9f4c
jzubietas <jhonathanisaizubietasantos@gmail.com>
2022-12-20 19:31:38 -0500

    a

--- app/Exports/Templates/Sheets/PagerutaenvioLimaSur.php
+++ app/Exports/Templates/Sheets/PagerutaenvioLimaSur.php
@@ -9,6 +9,7 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\BeforeSheet;
 use Maatwebsite\Excel\Events\AfterSheet;
 use Maatwebsite\Excel\Sheet;
 use PhpOffice\PhpSpreadsheet\Style\Fill;
