b3521f8f6b592e3a0bc67d065bd6db5007e98d05
jzubietas <jhonathanisaizubietasantos@gmail.com>
2022-12-20 19:41:06 -0500

    a

--- app/Exports/Templates/Sheets/PagerutaenvioLimaSinasignar.php
+++ app/Exports/Templates/Sheets/PagerutaenvioLimaSinasignar.php
@@ -154,7 +154,7 @@ class PagerutaenvioLimaSinasignar extends Export implements WithEvents,WithColum
 
     public static function beforeSheet(BeforeSheet $event){
         $event->sheet->appendRows(array(
-            array('', '','','','FECHA '.$fecharuta),
+            array('', '','','','FECHA '.$fecha),
             array('', '','','',''),
             //....
         ), $event);
