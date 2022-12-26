0c067ec77ebd5c2c84456d9ca8d59d84b2986609
jzubietas <jhonathanisaizubietasantos@gmail.com>
2022-12-26 16:04:36 -0500

    calculo +-3

--- app/Http/Controllers/MovimientoController.php
+++ app/Http/Controllers/MovimientoController.php
@@ -190,8 +190,8 @@ class MovimientoController extends Controller
             $monto_ma_3= floatval($monto_compara)+3;
             $monto_me_3= floatval($monto_compara)-3;
 
-            $query->where('movimiento_bancarios.ismporte','<=',$monto_ma_3);
             $query->where('movimiento_bancarios.importe','>=',$monto_me_3);
+            $query->where('movimiento_bancarios.importe','<=',$monto_ma_3);
             //$query->whereBetween('movimiento_bancarios.importe',[$monto_me_3,$monto_ma_3]);
 
 
