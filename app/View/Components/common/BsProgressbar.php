c07a41ea597cad629f71ac01cd7b87553c471e99
Luis sml <iluismejialeon@gmail.com>
2022-12-27 12:55:36 -0500

    fixed progress

---
+++ app/View/Components/common/BsProgressbar.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components\common;
+
+use Illuminate\View\Component;
+
+class BsProgressbar extends Component
+{
+    public $progress=0;
+
+    /**
+     * @param int $progress
+     */
+    public function __construct(int $progress)
+    {
+        $this->progress = $progress;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.common.bs-progressbar');
+    }
+}
