9c7a9128a2a7d9036ba91c486c523ee95d724591
Luis sml <iluismejialeon@gmail.com>
2022-12-27 14:02:35 -0500

    rol formacion

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,6 +35,7 @@ class User extends Authenticatable
     const ROL_JEFE_LLAMADAS = "Jefe de llamadas";
     const ROL_ASISTENTE_PAGOS = "Asistente de Pagos";
     const ROL_APOYO_ADMINISTRATIVO = "Apoyo administrativo";
+    const ROL_FORMACION = "FORMACIÓN";
 
 
     /**
