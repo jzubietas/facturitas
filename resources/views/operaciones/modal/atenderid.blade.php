2cc4b03d829e7a4b4760084019795de98c16ac07
jzubietas <jhonathanisaizubietasantos@gmail.com>
2022-12-28 12:06:35 -0500

    main boton cargaradjunto

--- resources/views/operaciones/modal/atenderid.blade.php
+++ resources/views/operaciones/modal/atenderid.blade.php
@@ -35,7 +35,7 @@
                               {!! Form::file('adjunto[]', ['class' => 'form-control-file', 'id'=>'adjunto', 'multiple']) !!}
                               {{-- <td>@csrf<input type="file" id="adjunto" name="adjunto[]" multiple=""/></td> --}}
 
-                              <button type="button" class="btn btn-primary" id="cargar_adjunto">Subir Informacion</button>
+                              
                           </div>
 
                           <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
@@ -45,6 +45,11 @@
                           </div>
 
                           <hr>
+                        </div>
+                        <div class="row">
+                          <div class="col-md-12 col-sm-12 col-xs-12">
+                            <button type="button" class="btn btn-primary" id="cargar_adjunto">Subir Informacion</button>
+                          </div>
                         </div>
                         <div class="row">
 
