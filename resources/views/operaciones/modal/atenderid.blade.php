7f5d471f4e5c95821e5cdb4653ae192b5607252c
jzubietas <jhonathanisaizubietasantos@gmail.com>
2022-12-28 11:36:20 -0500

    carga atenciones

--- resources/views/operaciones/modal/atenderid.blade.php
+++ resources/views/operaciones/modal/atenderid.blade.php
@@ -29,9 +29,6 @@
 
                         <div class="row">
 
-
-
-
                           <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
                               {!! Form::label('envio_doc', 'Documento enviado') !!}
                               @csrf
@@ -44,6 +41,12 @@
                               {!! Form::number('cant_compro', '', ['class' => 'form-control', 'id' => 'cant_compro', 'step'=>'1', 'min' => '0']) !!}
 
                           </div>
+
+                          <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                            <h6><b>Archivos adjuntos:</b></h6>
+                          </div>
+                          <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12" id="listado_adjuntos"></div>
+                          
                           <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
                             {!! Form::label('condicion', 'Estado') !!}
 
