d04e3d19dff0defbde64e0c827a0c9b5cf141c21
TeoEstudio <dei.guiribalde@gmail.com>
2022-12-29 15:07:06 -0500

    Inclusion de nuevo estado Pedido

--- routes/web.php
+++ routes/web.php
@@ -273,6 +273,7 @@ Route::middleware(['auth:sanctum', 'verified', 'auth.redirect.is_disabled'])->gr
     Route::get('operaciones.showatender/{pedido}', [OperacionController::class, 'showAtender'])->name('operaciones.showatender');
     Route::post('operaciones.recepcionid', [EnvioController::class, 'confirmarRecepcionID'])->name('operaciones.recepcionid');
     Route::post('operaciones.atender_pedido_op', [EnvioController::class, 'AtenderPedidoOP'])->name('operaciones.atender_pedido_op');
+    Route::post('operaciones.recibir_pedido_op', [EnvioController::class, 'RecibirPedidoOP'])->name('operaciones.recibir_pedido_op');
     Route::post('operaciones.envioid', [EnvioController::class, 'Enviarid'])->name('operaciones.envioid');
     Route::post('operaciones.revertirenvioid', [OperacionController::class, 'Revertirenvio'])->name('operaciones.revertirenvioid');
     Route::post('operaciones.sinenvioid', [EnvioController::class, 'SinEnviarid'])->name('operaciones.sinenvioid');
