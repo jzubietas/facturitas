9ec81b616a9c3b642a3ae3dc2ddda1d54bb415c0
Luis sml <iluismejialeon@gmail.com>
2022-12-29 09:51:25 -0500

    confirmar pedido y adjuntos

--- resources/views/pedidos/modal/ContenidoModal/ListadoAdjuntosSinConfirmar.blade.php
+++ resources/views/pedidos/modal/ContenidoModal/ListadoAdjuntosSinConfirmar.blade.php
@@ -1,23 +1,21 @@
+<input type="hidden" id="adjunto_total_attachment" value="{{count($imagenes)}}">
 <table>
     @foreach($imagenes as $img)
-        @if ($img->pedido_id == $pedido->id)
-            <tr class="adjuntos" data-adjunto="{{ $img->adjunto }}">
-                <td>
-                    <a target="_blank" download href="{{ \Storage::disk('pstorage')->url('adjuntos/'. $img->adjunto) }}">
-                        <span class="text-info">{{ $img->adjunto }}<span>
-                    </a>
-                </td>
-                <td><a href="#" style="margin-left: 12px;"  class="d-none" data-imgid="{{ $img->pedido_id }}" data-imgadjunto="{{ $img->adjunto }}"></a>
-
-                <a href="" data-target="#modal-delete-adjunto" data-imgid="{{ $img->pedido_id }}" data-imgadjunto="{{ $img->id }}" data-imgadjuntoconfirm="{{ $img->confirm }}" data-toggle="modal">
-                    <button class="btn-delete-adjunto btn btn-danger btn-sm" ><i class="fas fa-trash-alt"></i></button>
+        <tr class="adjuntos" data-adjunto="{{ $img->adjunto }}">
+            <td>
+                <a target="_blank" download href="{{ \Storage::disk('pstorage')->url('adjuntos/'. $img->adjunto) }}">
+                    <span class="text-info">{{ $img->adjunto }}</span>
                 </a>
-
-                    
-                </td>
-            </tr>
-        @endif
-
+            </td>
+            <td>
+                <a href="#" style="margin-left: 12px;" class="d-none" data-imgid="{{ $img->pedido_id }}"
+                   data-imgadjunto="{{ $img->adjunto }}"></a>
+                <a href="" data-target="#modal-delete-adjunto" data-imgid="{{ $img->pedido_id }}"
+                   data-imgadjunto="{{ $img->id }}" data-imgadjuntoconfirm="{{ $img->confirm }}" data-toggle="modal">
+                    <button class="btn-delete-adjunto btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></button>
+                </a>
+            </td>
+        </tr>
     @endforeach
 </table>
 
