6bf11b1a2c40802c36bee506b0c51845874ff3b3
jzubietas <jhonathanisaizubietasantos@gmail.com>
2022-12-29 23:21:03 -0500

    a

--- resources/views/sobres/modal/direccionid.blade.php
+++ resources/views/sobres/modal/direccionid.blade.php
@@ -81,6 +81,7 @@
                         <i class="fa fa-phone" aria-hidden="true"></i>
 
                         {!! Form::label('celular', 'Telefono del contacto quien recibe') !!}
+                        <span class="badge badge-pill badge-secondary">9 digitos</span>
                         {!! Form::number('celular', null, ['class' => 'form-control', 'id' => 'celular', 'min' =>'0', 'max' => '999999999', 'maxlength' => '9', 'oninput' => 'maxLengthCheck(this)']) !!}
                       </div>
 
