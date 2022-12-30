a44c7e4a0d6edfcbe397cb7e014675182ef076ba
jzubietas <jhonathanisaizubietasantos@gmail.com>
2022-12-29 21:53:56 -0500

    a

--- app/Http/Controllers/OperacionController.php
+++ app/Http/Controllers/OperacionController.php
@@ -215,7 +215,7 @@ class OperacionController extends Controller
             )
             ->where('pedidos.estado', '1')
             ->where('dp.estado', '1')
-            ->where('pedidos.condicion_code', Pedido::ATENDIDO_INT)
+            ->where('pedidos.condicion_envio_code', Pedido::ATENDIDO_OPE_INT)
             ->where('pedidos.envio', 0);
 
         if (Auth::user()->rol == "Operario") {
@@ -435,7 +435,17 @@ class OperacionController extends Controller
             )
             ->where('pedidos.estado', '1')
             ->where('dp.estado', '1')
-            ->whereIn('pedidos.condicion_envio_code', [Pedido::RECEPCION_COURIER_INT, Pedido::REPARTO_COURIER_INT, Pedido::SEGUIMIENTO_PROVINCIA_COURIER_INT, Pedido::ENTREGADO_CLIENTE_INT, Pedido::RECEPCION_COURIER_INT, Pedido::ENVIO_COURIER_JEFE_OPE_INT, Pedido::ENTREGADO_SIN_SOBRE_OPE_INT, Pedido::ENTREGADO_SIN_SOBRE_CLIENTE_INT]);
+            ->whereIn('pedidos.condicion_envio_code', [
+                Pedido::RECEPCION_COURIER_INT
+                , Pedido::REPARTO_COURIER_INT
+                , Pedido::SEGUIMIENTO_PROVINCIA_COURIER_INT
+                , Pedido::ENTREGADO_CLIENTE_INT
+                , Pedido::RECEPCION_COURIER_INT
+                , Pedido::ENVIO_COURIER_JEFE_OPE_INT
+                , Pedido::ENTREGADO_SIN_SOBRE_OPE_INT
+                , Pedido::ENTREGADO_SIN_SOBRE_CLIENTE_INT
+                , Pedido::ATENDIDO_OPE_INT
+            ]);
         //->whereIn('pedidos.condicion_envio_code', [Pedido::JEFE_OP_CONF_INT],[Pedido::COURIER_INT], [Pedido::EN_REPARTO_INT],[Pedido::SOBRE_ENVIAR_INT])
 
         //->whereIn('pedidos.envio', ['0'])
@@ -640,7 +650,7 @@ class OperacionController extends Controller
         }
 
         $pedido->update([
-            'condicion' => Pedido::$estadosCondicionCode[$request->condicion],
+            'condicion' => Pedido::$estadosCondicionEnvioCode[$request->condicion],
             'condicion_code' => $request->condicion,
             'condicion_envio' => Pedido::$estadosCondicionEnvioCode[$request->condicion],
             'condicion_envio_code' => $request->condicion,
