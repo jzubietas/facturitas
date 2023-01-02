c736814c4df4803c0cc8405deb7bc4114ab37ea8
TeoEstudio <dei.guiribalde@gmail.com>
2023-01-02 14:18:49 -0500

    Update notificacion controller

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -291,8 +291,8 @@ class NotificationsController extends Controller
             ->join('detalle_pedidos as dp', 'pedidos.id', 'dp.pedido_id')
             ->where('pedidos.estado', '1')
             ->where('dp.estado', '1')
-            ->where('pedidos.condicion_code', Pedido::ATENDIDO_OPE_INT)
-            ->where('pedidos.envio', 0);
+            ->where('pedidos.condicion_envio_code', Pedido::ATENDIDO_OPE_INT);
+            //->where('pedidos.envio', 0);
 
         if(Auth::user()->rol == User::ROL_ASESOR){
 
@@ -357,6 +357,8 @@ class NotificationsController extends Controller
         }
 
         $contador_pedidos_atendidos_operacion = $contador_pedidos_atendidos_operacion->count();
+
+
         /************
          * Pedidos pendientes de anulación
          */
