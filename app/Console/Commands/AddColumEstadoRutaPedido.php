df2cfd7f5ea10973c4fda8ffedf65d28735a7bb4
jzubietas <jhonathanisaizubietasantos@gmail.com>
2023-01-05 16:23:28 -0500

    comando ruta

---
+++ app/Console/Commands/AddColumEstadoRutaPedido.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Pago;
+use App\Models\Pedido;
+use Illuminate\Console\Command;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddColumnEstadoRutaPedido extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'migrate:add-ruta-pedido';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        \Schema::table('pedidos', function (Blueprint $table) {
+            $table->integer("estado_ruta")->nullable()->after('estado_sobre');
+        });
+
+        return 0;
+    }
+}
