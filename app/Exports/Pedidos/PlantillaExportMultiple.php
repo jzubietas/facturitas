ae17fbd73092790ceb7aaf3d6b3e9ec41867cdf0
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-01-10 09:43:56 -0500

    Update PlantillaExportMultiple.php

--- app/Exports/Pedidos/PlantillaExportMultiple.php
+++ app/Exports/Pedidos/PlantillaExportMultiple.php
@@ -2,18 +2,7 @@
 
 namespace App\Exports\Templates;
 
-use App\Exports\Templates\Sheets\PageclienteInfo;
-use App\Exports\Templates\Sheets\PageclienteSituacion;
-//use App\Exports\Templates\Sheets\PageclienteOctubre;
-//use App\Exports\Templates\Sheets\PageclienteinfoSetiembre;
-//use App\Exports\Templates\Sheets\PageclienteinfoAgosto;
-//use App\Exports\Templates\Sheets\PageclienteinfoJulio;
-//use App\Exports\Templates\Sheets\PageclienteinfoOctubre;
-//use App\Exports\Templates\Sheets\PageclienteNoviembre;
-use App\Exports\Templates\Sheets\PageclienteinfoNoviembre;
-//use App\Exports\Templates\Sheets\PageclienteDiciembre;
-use App\Exports\Templates\Sheets\PageclienteinfoDiciembre;
-use App\Exports\Templates\Sheets\PageclienteinfoEnero2023;
+use App\Exports\Templates\Sheets\PagepedidosInfo;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 use Illuminate\Http\Request;
