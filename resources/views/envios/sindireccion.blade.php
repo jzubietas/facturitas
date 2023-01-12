a89de11fd7e35ed11d63770207533b14f6cc5926
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-01-11 22:49:54 -0500

    a

--- resources/views/envios/sindireccion.blade.php
+++ resources/views/envios/sindireccion.blade.php
@@ -350,8 +350,8 @@
             },
           },
           {
-            data: 'created_at',
-            name: 'created_at',
+            data: 'updated_at',
+            name: 'updated_at',
             //render: $.fn.dataTable.render.moment( 'DD/MM/YYYY' )
            },
           {
