f7a11ddba17be11634f3056ffc25cd43b19c5c7e
Luis ML <iluismejialeon@gmail.com>
2023-01-12 14:22:55 -0500

    fixed

--- app/Exports/Templates/Sheets/PagerecepcionMotorizado.php
+++ app/Exports/Templates/Sheets/PagerecepcionMotorizado.php
@@ -28,17 +28,19 @@ Sheet::macro('styleCells', function (Sheet $sheet, string $cellRange, array $sty
 
 class PagerecepcionMotorizado extends Export implements WithColumnFormatting, FromCollection, WithHeadings, ShouldAutoSize, WithEvents
 {
-    public int $user_motorizado_h=0;
-    public string $fecha_envio_h='';
-    public function __construct($user_motorizado_p,$fecha_envio_p)
+    public int $motorizado_id = 0;
+    public string $fecha_envio_h = '';
+
+    public function __construct($user_motorizado_p, $fecha_envio_p)
     {
         parent::__construct();
-        $this->user_motorizado_h=$user_motorizado_p;
-        $this->fecha_envio_h=$fecha_envio_p;
+        $this->motorizado_id = $user_motorizado_p;
+        $this->fecha_envio_h = $fecha_envio_p;
     }
+
     public function collection()
     {
-        $direccion=DireccionGrupo::where('direccion_grupos.estado','1')
+        $direccion = DireccionGrupo::where('direccion_grupos.estado', '1')
             ->join('users as u', 'direccion_grupos.user_id', 'u.id')
             ->join('clientes as c', 'direccion_grupos.cliente_id', 'c.id')
             ->select([
@@ -53,35 +55,35 @@ class PagerecepcionMotorizado extends Export implements WithColumnFormatting, Fr
                 'direccion_grupos.referencia',
                 'direccion_grupos.condicion_envio',
             ]);
-        if(!$this->user_motorizado_h){
-            $direccion=$direccion->where('direccion_grupos.motorizado_id',$this->user_motorizado_h);
+        if (!$this->motorizado_id) {
+            $direccion = $direccion->where('direccion_grupos.motorizado_id', $this->motorizado_id);
         }
-        if(!$this->fecha_envio_h){
-            $direccion=$direccion->where('cast(direccion_grupos.fecha_recepcion as date)',$this->fecha_envio_h);
+        if (!$this->fecha_envio_h) {
+            $direccion = $direccion->where('cast(direccion_grupos.fecha_recepcion as date)', $this->fecha_envio_h);
         }
 
-        return  $direccion->get();
+        return $direccion->get();
     }
 
     public function title(): string
     {
         //return parent::title();//Por defecto se toma del nombre de la clase de php, en este caso seria "Pagina One" de titulo
-        return 'Recepcion Motorizado '.$this->user_motorizado_h.' '.$this->fecha_envio_h;
+        return 'Motorizado ' . $this->motorizado_id . ' ' . $this->fecha_envio_h;
     }
 
     public function columnWidths(): array
     {
         return [
             'A' => 8 //ITEM
-            ,'B' => 8 //CODIGO
-            ,'C' => 8 //ASESOR
-            ,'D' => 8 //Cliente
-            ,'E' => 8 //FECHA ENVIO
-            ,'F' => 8 //RAZON SOCIAL
-            ,'G' => 8 //DESTINO
-            ,'H' => 8 //DIRECCION ENVIO
-            ,'I' => 8 //REFERENCIA
-            ,'J' => 8 //ESTADO DE ENVIO
+            , 'B' => 8 //CODIGO
+            , 'C' => 8 //ASESOR
+            , 'D' => 8 //Cliente
+            , 'E' => 8 //FECHA ENVIO
+            , 'F' => 8 //RAZON SOCIAL
+            , 'G' => 8 //DESTINO
+            , 'H' => 8 //DIRECCION ENVIO
+            , 'I' => 8 //REFERENCIA
+            , 'J' => 8 //ESTADO DE ENVIO
         ];
     }
 
@@ -89,27 +91,30 @@ class PagerecepcionMotorizado extends Export implements WithColumnFormatting, Fr
     {
         return parent::map($model);
     }
+
     public function fields(): array
     {
         return [
-            "correlativo"=>"ITEM"
-            ,"codigos"=>"Codigo"
-            ,"identificador"=>"Asesor"
-            ,"nombre"=>"Cliente"
-            ,"fecha_recepcion"=>"Fecha de Envio"
-            ,"producto"=>"Razon Social"
-            ,"destino"=>"Destino"
-            ,"direccion"=>"Direccion de Envio"
-            ,"referencia"=>"Referencia"
-            ,"condicion_envio"=>"Estado de Envio"
+            "correlativo" => "ITEM"
+            , "codigos" => "Codigo"
+            , "identificador" => "Asesor"
+            , "nombre" => "Cliente"
+            , "fecha_recepcion" => "Fecha de Envio"
+            , "producto" => "Razon Social"
+            , "destino" => "Destino"
+            , "direccion" => "Direccion de Envio"
+            , "referencia" => "Referencia"
+            , "condicion_envio" => "Estado de Envio"
         ];
     }
+
     public function columnFormats(): array
     {
         return [
             'E' => NumberFormat::FORMAT_DATE_YYYYMMDD
         ];
     }
+
     public function registerEvents(): array
     {
         return [
@@ -117,29 +122,28 @@ class PagerecepcionMotorizado extends Export implements WithColumnFormatting, Fr
         ];
     }
 
-    public static function afterSheet(AfterSheet $event){
-
-
-        $color_recurente='a9def9';
-        $color_recuperadoabandono='3a86ff';
-        $color_recuperadoreciente='00b4d8';
-        $color_nuevo='b5e48c';
-        $color_basefria='ffffff';
-        $color_abandono='d62828';
-        $color_abandonoreciente='fca311';
-        $color_default='eff7f6';
+    public static function afterSheet(AfterSheet $event)
+    {
+        $color_recurente = 'a9def9';
+        $color_recuperadoabandono = '3a86ff';
+        $color_recuperadoreciente = '00b4d8';
+        $color_nuevo = 'b5e48c';
+        $color_basefria = 'ffffff';
+        $color_abandono = 'd62828';
+        $color_abandonoreciente = 'fca311';
+        $color_default = 'eff7f6';
 
         $style_recurrente = array(
-                'fill' => array(
-                    'fillType' => Fill::FILL_SOLID,
-                    'startColor' => array('argb' => $color_recurente)
-                )
+            'fill' => array(
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => array('argb' => $color_recurente)
+            )
         );
         $style_recuperadoabandono = array(
-                'fill' => array(
-                    'fillType' => Fill::FILL_SOLID,
-                    'startColor' => array('argb' => $color_recuperadoabandono)
-                )
+            'fill' => array(
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => array('argb' => $color_recuperadoabandono)
+            )
         );
         $style_recuperadoreciente = array(
             'fill' => array(
@@ -178,65 +182,9 @@ class PagerecepcionMotorizado extends Export implements WithColumnFormatting, Fr
             )
         );
 
-        $row_cell_=14;
-        $letter_cell='N';
-        foreach ($event->sheet->getRowIterator() as $row)
-        {
-            if($row->getRowIndex()==1)continue;
-            if($event->sheet->getCellByColumnAndRow($row_cell_,$row->getRowIndex())->getValue()=='RECURRENTE')
-            {
-                $event->sheet->getStyle($letter_cell.$row->getRowIndex())->applyFromArray($style_recurrente);
-            }
-            else if($event->sheet->getCellByColumnAndRow($row_cell_,$row->getRowIndex())->getValue()=='RECUPERADO ABANDONO')
-            {
-                $event->sheet->getStyle($letter_cell.$row->getRowIndex())->applyFromArray($style_recuperadoabandono);
-            }
-            else if($event->sheet->getCellByColumnAndRow($row_cell_,$row->getRowIndex())->getValue()=='RECUPERADO RECIENTE')
-            {
-                $event->sheet->getStyle($letter_cell.$row->getRowIndex())->applyFromArray($style_recuperadoreciente);
-            }
-            else if($event->sheet->getCellByColumnAndRow($row_cell_,$row->getRowIndex())->getValue()=='NUEVO')
-            {
-                $event->sheet->getStyle($letter_cell.$row->getRowIndex())->applyFromArray($style_nuevo);
-            }
-            else if($event->sheet->getCellByColumnAndRow($row_cell_,$row->getRowIndex())->getValue()=='BASE FRIA')
-            {
-                $event->sheet->getStyle($letter_cell.$row->getRowIndex())->applyFromArray($style_basefria);
-            }
-            else if($event->sheet->getCellByColumnAndRow($row_cell_,$row->getRowIndex())->getValue()=='ABANDONO')
-            {
-                $event->sheet->getStyle($letter_cell.$row->getRowIndex())->applyFromArray($style_abandono);
-            }
-            else if($event->sheet->getCellByColumnAndRow($row_cell_,$row->getRowIndex())->getValue()=='ABANDONO RECIENTE')
-            {
-                $event->sheet->getStyle($letter_cell.$row->getRowIndex())->applyFromArray($style_abandonoreciente);
-            }else{
-                $event->sheet->getStyle($letter_cell.$row->getRowIndex())->applyFromArray($styledefault);
-            }
+        $row_cell_ = 14;
+        $letter_cell = 'N';
 
-        }
-
-        /*echo 'ROW: ', $cell->getRow(), PHP_EOL;
-                   echo 'COLUMN: ', $cell->getColumn(), PHP_EOL;
-                   echo 'COORDINATE: ', $cell->getCoordinate(), PHP_EOL;
-                   echo 'RAW VALUE: ', $cell->getValue(), PHP_EOL;*/
-
-        //Range Columns
-                /*
-                $event->sheet->styleCells(
-                    'Q',
-                    [
-                        'alignment' => [
-                            'horizontal' => Alignment::HORIZONTAL_CENTER,
-                        ],
-                        'fill' => [
-                            'fillType' => Fill::FILL_SOLID,
-                            'color' => ['rgb' => '336655']
-                        ]
-                    ]
-                ); */
-            }
-
-
+    }
 }
 
