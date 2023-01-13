56e36a7423b723b07c44df1b453ecb239ce0cf21
jzubietas <jhonathanisaizubietasantos@gmail.com>
2023-01-13 14:33:01 -0500

    Update de ruta

--- resources/views/envios/modal/confirmacion.blade.php
+++ resources/views/envios/modal/confirmacion.blade.php
@@ -22,8 +22,9 @@
 
                 <div class="row">
                   <div class="col">
-                    {!! Form::label('fecha_salida', 'Fecha de ruta') !!}
-                    {!! Form::date('fecha_salida', \Carbon\Carbon::now()->addDays(1), ['class' => 'form-control', 'id' => 'fecha_salida']) !!}
+                      {!! Form::label('fecha_salida', 'Fecha de ruta') !!}
+                    <!--{!! Form::date('fecha_salida', \Carbon\Carbon::now()->addDays(1), ['class' => 'form-control', 'id' => 'fecha_salida']) !!}-->
+                      {!! Form::date('fecha_salida', \Carbon\Carbon::now(), ['class' => 'form-control', 'id' => 'fecha_salida']) !!}
                   </div>
                 </div>
 
