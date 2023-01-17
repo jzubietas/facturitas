b53365993b8798e438b691b5bbeb61fee0b36148
Luis ML <iluismejialeon@gmail.com>
2023-01-16 23:23:06 -0500

    fixed

--- resources/views/rotulospdf.blade.php
+++ resources/views/rotulospdf.blade.php
@@ -1,24 +1,49 @@
 @foreach($rotulos as $rotulo)
-<table style="@if($loop->index==1)
+    <table style="@if($loop->index==1)
 page-break-before: always;
 page-break-after: always;
     @elseif($loop->index>1)
     page-break-after: always;
 @endif">
-    <tr>
-        <td>
-            {{join(',',$rotulo['codigos'])}}
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <img src="{{$rotulo['file']}}">
-        </td>
-    </tr>
-    <tr>
-        <td>
-            {{join(',',$rotulo['producto'])}}
-        </td>
-    </tr>
-</table>
+        <tr>
+            <td>
+
+            </td>
+            <td>
+                <table style="width: 100%">
+                    @foreach(collect($rotulo['codigos'])->chunk(2) as $grupos)
+                        <tr>
+                            @foreach($grupos as $item)
+                                <td>
+                                    <b>{{$item}}</b>
+                                </td>
+                            @endforeach
+                        </tr>
+                    @endforeach
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <img src="{{$rotulo['file']}}">
+            </td>
+        </tr>
+        <tr>
+            <td>
+            </td>
+            <td>
+                <table style="width: 100%">
+                    @foreach(collect($rotulo['producto'])->chunk(2) as $grupos)
+                        <tr>
+                            @foreach($grupos as $item)
+                                <td>
+                                    <b>{{$item}}</b>
+                                </td>
+                            @endforeach
+                        </tr>
+                    @endforeach
+                </table>
+            </td>
+        </tr>
+    </table>
 @endforeach
