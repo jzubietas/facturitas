6fac20469eb595148a16942261bf320dcc0dd747
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-01-18 21:50:07 -0500

    a

---
+++ app/Console/Commands/MasivaCodigoProductoDireccionGrupo.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\DireccionGrupo;
+use App\Models\Pedido;
+use Illuminate\Console\Command;
+
+class MasivaCodigoProductoDireccionGrupo extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'masiva:codigoproducto:direcciongrupo';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $grupos = DireccionGrupo::query()->activo()
+            //->where('pedidos.condicion_envio_code', '=', Pedido::ENVIO_COURIER_JEFE_OPE_INT)
+            ->get();
+        $count = $grupos->count();
+        $result = [];
+        $progress = $this->output->createProgressBar($count);
+        foreach ($grupos as $grupo)
+        {
+            DireccionGrupo::restructurarCodigos($grupo);
+            $progress->advance();
+        }
+        $progress->finish();
+
+        return 0;
+    }
+}
