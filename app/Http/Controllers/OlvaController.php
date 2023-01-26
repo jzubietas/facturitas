72666ed9bfc59bb68c19235ba5af3c47d76d88ab
Luis ML <iluismejialeon@gmail.com>
2023-01-26 16:39:41 -0500

    g

--- app/Http/Controllers/OlvaController.php
+++ app/Http/Controllers/OlvaController.php
@@ -398,7 +398,7 @@ class OlvaController extends Controller
     {
         return response()->json([
             'grupo' => $grupo,
-            'data' => $grupo->getMedia('tienda_olva_notificado')->toArray(),
+            'data' => $grupo->getMedia('tienda_olva_notificado')->sortByDesc('created_at_format')->toArray(),
         ]);
     }
 
