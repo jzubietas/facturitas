5229e965620a08242dbddde3c31012c35f5e8bee
Luis ML <iluismejialeon@gmail.com>
2023-01-26 14:23:09 -0500

    fixed olva

---
+++ app/Console/Commands/normalizar/OlvaTracking.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands\normalizar;
+
+use App\Models\DireccionGrupo;
+use Illuminate\Console\Command;
+
+class OlvaTracking extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'normalizar:olva-tracking-format';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $grupos = DireccionGrupo::query()->inOlva()->whereNotNull('courier_failed_sync_at')->get();
+        foreach ($grupos as $grupo) {
+            $code = $grupo->direccion;
+            if(\Str::contains($code,'-')){
+                continue;
+            }
+            $numreg = $grupo->referencia;
+            if ($numreg && $code) {
+                if (strlen($numreg) < strlen($code)) {
+                    $code=$grupo->referencia;
+                    $numreg=$grupo->direccion;
+                    $grupo->update([
+                       'referencia' => $numreg,
+                        'direccion' => $code
+                    ]);
+                }
+            }
+            $year = (int)substr($code, strlen($code) - 2, 2);
+            $code = substr($code, 0, strlen($code) - strlen($year));
+            $cyear = (int)now()->format('y');
+            if (strlen($code) >= 4 && $year > 19 && $year <= $cyear) {
+                $this->info("$code - $year");
+                $grupo->update([
+                    'direccion' => $code . '-' . $year,
+                    'courier_failed_sync_at' => null
+                ]);
+            }
+        }
+        return 0;
+    }
+}
