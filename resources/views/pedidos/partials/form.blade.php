bab094bf353655de28a65e4ad3fd2d6942b78423
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-01-27 14:37:14 -0500

    a

--- resources/views/pedidos/partials/form.blade.php
+++ resources/views/pedidos/partials/form.blade.php
@@ -217,8 +217,17 @@
                     </div>
                 </div>
 
+
+
                 <div class="row">
-                    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12" style="text-align: center">
+                    <div class="col-lg-9 col-sm-9 col-md-9 col-xs-9" style="text-align: center">
+
+                        <div class="alert alert-warning" role="alert">
+                            Aqui se ingresan los campos para envios destino
+                        </div>
+
+                    </div>
+                    <div class="col-lg-3 col-sm-3 col-md-3 col-xs-3" style="text-align: center">
                         <button data-backdrop="static" data-keyboard="false" data-toggle="modal"
                                 data-target="#modal-direccion_crearpedido" type="button"
                                 id="bt_add_dir" class="float-right btn btn-success btn-lg d-none"><i
