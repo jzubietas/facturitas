f88b6d7f41b416db4dfb3e2f83eb557b5ba12f8c
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-01-28 13:47:55 -0500

    a

--- routes/web.php
+++ routes/web.php
@@ -460,7 +460,7 @@ Route::middleware(['auth:sanctum', 'verified', 'auth.redirect.is_disabled'])->gr
     Route::get('sobreenvioshistorial', [SobreController::class, 'sobreenvioshistorial'])->name('sobreenvioshistorial');
     /*Controller Sobres*/
 
-    Route::get('ajax_modal1_response', [ModalController::class, 'ajax_modal1_response'])->name('ajax_modal1_response');
+    Route::any('ajax_modal1_response', [ModalController::class, 'ajax_modal1_response'])->name('ajax_modal1_response');
 
     //Route::post('envios.recibir/{pedido}', [PedidoController::class, 'Recibir'])->name('envios.recibir');
     //Route::post('envios.enviar/{pedido}', [PedidoController::class, 'EnviarPedido'])->name('envios.enviar');
