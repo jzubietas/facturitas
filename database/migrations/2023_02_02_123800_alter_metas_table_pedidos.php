9dcfffb74a2b7c65e54ee3933890ba8d17512cc6
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-02-02 12:39:28 -0500

    a

---
+++ database/migrations/2023_02_02_123800_alter_metas_table_pedidos.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterMetasTablePedidos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('metas',function (Blueprint $table){
+            if(!Schema::hasColumn('metas','meta_pedido'))
+            {
+                $table->int('meta_pedido')->nullable()->comment('')->after('mes');
+                $table->int('meta_cobro')->nullable()->comment('')->after('meta_pedido');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
