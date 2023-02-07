11b04e84b9ad1ebe2eb92fcafa2cba71cadb4841
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-02-07 14:48:04 -0500

    a

--- app/Http/Controllers/OperacionController.php
+++ app/Http/Controllers/OperacionController.php
@@ -600,7 +600,7 @@ class OperacionController extends Controller
                             <i class="fa fa-check"></i>
                         CORREGIR
                         </a>';
-                $btn []= '<a href="' . route('correccionpedidoPDF', data_get($pedido, 'pedido_id')) . '" class="btn-sm dropdown-item py-2" target="_blank"><i class="fa fa-file-pdf text-primary"></i> Ver PDF</a>';
+                $btn []= '<a href="' . route('correccionPDF', data_get($pedido, 'pedido_id')) . '" class="btn-sm dropdown-item py-2" target="_blank"><i class="fa fa-file-pdf text-primary"></i> Ver PDF</a>';
                 /*$btn[] = '<a class="btn-sm dropdown-item text-danger" href="#"'.
                         'data-toggle="modal"'.
                         'data-correccion="'.$pedido->id.'"'.
