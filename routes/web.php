a5a9200a4d82c3d6eb501b0d6865f9f81778c475
Renzoi10 <65270435+Renzoi10@users.noreply.github.com>
2023-02-11 13:11:27 -0500

    a

--- routes/web.php
+++ routes/web.php
@@ -711,6 +711,9 @@ Route::middleware(['auth:sanctum', 'verified', 'auth.redirect.is_disabled'])->gr
     Route::get('direcciongrupo/{grupo}/no_contesto/get_sustentos_adjuntos', [DireccionGrupoController::class, 'get_sustentos_adjuntos'])->name('direcciongrupo.no-contesto.get-sustentos-adjuntos');
 
 
+    Route::post('agregardireccionjefeoperaciones', [SettingsController::class, 'agregardireccionjefeoperaciones'])->name('agregardireccionjefeoperaciones.post');
+
+
     /* Route::group(['middleware' => ['permission:pedidos.index']], function () {
         Route::get('pedidos.index', [PedidoController::class, 'index']);
     }); */
