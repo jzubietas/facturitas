4ab346f35528209767e11e9dd23dc20fb10e26d8
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-02-17 20:46:22 -0500

    a

--- resources/views/dashboard/partials/vista_administrador.blade.php
+++ resources/views/dashboard/partials/vista_administrador.blade.php
@@ -450,6 +450,14 @@
         cargaNueva(2);
         cargaNueva(3);
 
+        setInterval(myTimer, 5000);
+
+        function myTimer() {
+          cargaNueva(1);
+          cargaNueva(2);
+          cargaNueva(3);
+        }
+
         $('a[href$="#myModal"]').on( "click", function() {
           $('#myModal').modal();
         });
