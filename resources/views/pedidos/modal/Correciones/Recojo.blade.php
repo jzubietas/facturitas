f9c6f073c6f29b3a3edafa145078d0b2663aea5f
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-02-18 12:29:40 -0500

    a

--- resources/views/pedidos/modal/Correciones/Recojo.blade.php
+++ resources/views/pedidos/modal/Correciones/Recojo.blade.php
@@ -13,20 +13,26 @@
                   <input type="hidden" name="opcion" value="4">
 
                   <div class="row">
-                    <div class="col">
-                      <div class="form-group mx-sm-3 mb-2 col-4 mt-3">
-                        <input type="hidden" class="form-control" id="Cliente" >
-                        <input type="text" class="form-control" id="cod_Cliente"  >
+                    <div class="col-md-6">
+                      <div class="form-row">
+                        <div class="form-group">
+                          <input type="hidden" class="form-control" id="Cliente" >
+                          <input type="text" class="form-control" id="cod_Cliente"  >
+                        </div>
                       </div>
+
                     </div>
-                    <div class="col">
-                      <div class="form-group mx-sm-3 mb-2 col-4 mt-3">
-                        <input type="hidden" class="form-control" id="Id-Cliente">
-                        <input type="text" class="form-control" id="cod_pedido"  >
+                    <div class="col-md-4">
+                      <div class="form-row">
+                        <div class="form-group">
+                          <input type="hidden" class="form-control" id="Id-Cliente">
+                          <input type="text" class="form-control" id="cod_pedido"  >
+                        </div>
                       </div>
+
                     </div>
-                    <div class="col">
-                      <button type="button" data-toggle="modal" data-backdrop="static" data-keyboard="false" class="btn-sm dropdown-item btn btn-primary mb-2 col-1 mt-3" data-target="#modal-listclientes" >+</button>
+                    <div class="col-md-2">
+                      <button type="button" data-toggle="modal" data-backdrop="static" data-keyboard="false" class="btn-sm btn btn-primary" data-target="#modal-listclientes" >+</button>
                     </div>
                   </div>
 
@@ -38,50 +44,53 @@
                           <i class="fa fa-user text-red" aria-hidden="true"></i>
                           <input type="hidden" name="nombre_id" id="Nombre_recibe">
                           {!! Form::label('nombre_recojo', 'Nombre del contacto quien recibe') !!}
-                          {!! Form::text('nombre_recojo', null, ['class' => 'form-recojo', 'placeholder' => 'Nombre', 'autocomplete' => 'off']) !!}
+                          {!! Form::text('nombre_recojo', null, ['class' => 'form-control', 'placeholder' => 'Nombre', 'autocomplete' => 'off']) !!}
                         </div>
                         <div class="form-group col-md-6">
                           <i class="fa fa-phone text-red" aria-hidden="true"></i>
                           {!! Form::label('celular', 'Telefono del contacto quien recibe') !!}
                           <span class="badge badge-pill badge-secondary">9 digitos</span>
-                          {!! Form::number('celular', null, ['class' => 'form-recojo', 'id' => 'celular_recojo', 'min' =>'0', 'max' => '999999999', 'maxlength' => '9', 'oninput' => 'maxLengthCheck(this)','placeholder' => '9 digitos']) !!}
+                          {!! Form::number('celular', null, ['class' => 'form-control', 'id' => 'celular_recojo', 'min' =>'0', 'max' => '999999999', 'maxlength' => '9', 'oninput' => 'maxLengthCheck(this)','placeholder' => '9 digitos']) !!}
                         </div>
 
                       </div>
 
-
-
-
-
-                      <div class="form-group col-md-6">
-                        <i class="fa fa-street-view text-red" aria-hidden="true"></i>
-
-                        {!! Form::label('direccion_recojo', 'Direccion') !!}
-                        {!! Form::text('direccion_recojo', null, ['class' => 'form-recojo', 'placeholder' => 'Direccion_recojo' , 'autocomplete' => 'off']) !!}
+                      <div class="form-row">
+                        <div class="form-group col-md-12">
+                          <i class="fa fa-street-view text-red" aria-hidden="true"></i>
+                          {!! Form::label('direccion_recojo', 'Direccion') !!}
+                          {!! Form::text('direccion_recojo', null, ['class' => 'form-control', 'placeholder' => 'Direccion_recojo' , 'autocomplete' => 'off']) !!}
+                        </div>
                       </div>
 
-                      <div class="form-group col-md-6">
-                        <i class="fa fa-commenting-o text-red" aria-hidden="true"></i>
-
-                        {!! Form::label('referencia_recojo', 'Referencia') !!}
-                        {!! Form::text('referencia_recojo', null, ['class' => 'form-recojo', 'placeholder' => 'Referencia_recojo' , 'autocomplete' => 'off']) !!}
+                      <div class="form-row">
+                        <div class="form-group col-md-12">
+                          <i class="fa fa-commenting-o text-red" aria-hidden="true"></i>
+                          {!! Form::label('referencia_recojo', 'Referencia') !!}
+                          {!! Form::text('referencia_recojo', null, ['class' => 'form-control', 'placeholder' => 'Referencia_recojo' , 'autocomplete' => 'off']) !!}
+                        </div>
                       </div>
 
-                      <div class="form-group col-md-12">
-                        {!! Form::label('observacion_recojo', 'Observacion') !!}
-                        {!! Form::text('observacion_recojo', null, ['class' => 'form-recojo', 'placeholder' => 'Observacion_recojo' , 'autocomplete' => 'off']) !!}
+                      <div class="form-row">
+                        <div class="form-group col-md-12">
+                          {!! Form::label('observacion_recojo', 'Observacion') !!}
+                          {!! Form::text('observacion_recojo', null, ['class' => 'form-control', 'placeholder' => 'Observacion_recojo' , 'autocomplete' => 'off']) !!}
+                        </div>
                       </div>
 
-                      <div class="form-group col-md-12">
-                        {!! Form::label('gmlink_recojo', 'Link Google Map') !!}
-                        {!! Form::text('gmlink_recojo', null, ['class' => 'form-recojo', 'placeholder' => 'Ejem: https://goo.gl/maps/*********' , 'autocomplete' => 'off']) !!}
+                      <div class="form-row">
+                        <div class="form-group col-md-12">
+                          {!! Form::label('gmlink_recojo', 'Link Google Map') !!}
+                          {!! Form::text('gmlink_recojo', null, ['class' => 'form-control', 'placeholder' => 'Ejem: https://goo.gl/maps/*********' , 'autocomplete' => 'off']) !!}
+                        </div>
                       </div>
 
-                      <div class="form-group col-md-12">
-                        <label for="Direccion_de_entrega">Direccion de entrega</label>
-                        <input type="text" class="form-control" id="Direccion_de_entrega" placeholder="" readonly>
+                      <div class="form-row">
+                        <div class="form-group col-md-12">
+                          <label for="Direccion_de_entrega">Direccion de entrega</label>
+                          <input type="text" class="form-control" id="Direccion_de_entrega" placeholder="" readonly>
+                        </div>
                       </div>
-
 
                     </div>
                   </div>
