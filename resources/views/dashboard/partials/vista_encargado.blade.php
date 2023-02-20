31d5de3f27a95df096ed61b1cd9cf44d8d6b5945
Junior Pezantes <werras04@gmail.com>
2023-02-20 08:33:44 -0500

    eliminacion de scripts

--- resources/views/dashboard/partials/vista_encargado.blade.php
+++ resources/views/dashboard/partials/vista_encargado.blade.php
@@ -216,8 +216,6 @@
       $(this).text($(this).attr("data-progress") + "%");
     });
   </script>
-  <script src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/1.13.2/js/dataTables.bootstrap4.min.js"></script>
   <script>
     $(document).ready(function () {
       $.ajaxSetup({
