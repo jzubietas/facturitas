f95536e69d2b4be5998ced8ca2981a388b93d769
Junior Pezantes <werras04@gmail.com>
2023-02-23 12:09:50 -0500

    Creacion de job y observer

---
+++ app/Jobs/PostUpdateSituacion.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Cliente;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+
+class PostUpdateSituacion
+{
+    use Dispatchable;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+  protected $cliente_id;
+  public function __construct($client_id)
+  {
+    $this->cliente_id = $client_id;
+  }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Cliente::createSituacionByCliente($this->cliente_id);
+    }
+}
--- app/Observers/PedidoObserver.php
+++ app/Observers/PedidoObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Jobs\PostCreatePedido;
 use App\Jobs\PostUpdatePedido;
+use App\Jobs\PostUpdateSituacion;
 use App\Models\Cliente;
 use App\Models\Pedido;
 use Illuminate\Support\Facades\DB;
@@ -20,6 +21,7 @@ class PedidoObserver
     {
         \Log::info("PostCreatePedido -> ".$pedido->cliente_id);
         PostCreatePedido::dispatchSync($pedido->cliente_id);
+        PostUpdateSituacion::dispatchSync($pedido->cliente_id);
     }
 
     /**
