3077fc9399dcf10690474805c2f6be9c0bfe6902
Agapito De la cruz <adelacruzcarlos@gmail.com>
2023-02-24 18:08:55 -0500

    diseño contacto
    diseño contacto

--- resources/views/layouts/modal/modal2.blade.php
+++ resources/views/layouts/modal/modal2.blade.php
@@ -1,16 +1,16 @@
 <div class="modal fade" id="modal-llamadas-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" style="max-width: 800px!important;">
     <div class="modal-content">
-      <div class="modal-header bg-success">
+      <div class="modal-header bg-gradient-info">
         <h5 class="modal-title" id="exampleModalLabel">Listado Clientes</h5>
 
-        <button class="float-right btn btn-danger" data-dismiss="modal">Cerrar</button>
+        <button class="float-right btn btn-danger" data-dismiss="modal"><i class="fas fa-times" aria-hidden="true" ></i></button>
       </div>
       <div class="modal-body">
 
         <div id="op-1-row" class="op-1-row"><!---->
           <table id="tablaListadoLlamadas" class="table table-striped w-100" style="text-align: center">
-            <thead><h4 style="text-align: center"><strong>Listado Clientes</strong></h4>
+            <thead></h4>
             <tr>
               <th scope="col">Asesor</th>
               <th scope="col">Celular</th>
