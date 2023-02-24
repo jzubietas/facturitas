b17f3af74638709fa82b8ec5d13ed809b91cddb6
Junior Pezantes <werras04@gmail.com>
2023-02-24 17:56:32 -0500

    a

--- app/Models/GrupoPedido.php
+++ app/Models/GrupoPedido.php
@@ -126,8 +126,8 @@ class GrupoPedido extends Model
                 'distrito' => optional($distrito)->distrito ?? data_get($array, 'distrito') ?? 'n/a',//LOS OLIVOS
                 'direccion' => data_get($array, 'direccion') ?: 'n/a',//olva
                 'cliente_recibe' => data_get($array, 'cliente_recibe') ?? 'n/a',//olva
-                'cod_recojo' => data_get($array,'cod_recojo'),
-                'env_sustento_recojo' => data_get($array,'env_sustento_recojo'),
+                'cod_recojo' => data_get($array,'cod_recojo') ?? 'n/a',
+                'env_sustento_recojo' => data_get($array,'env_sustento_recojo') ?? 'n/a',
             ];
             $data2 = [
                 'referencia' => data_get($array, 'referencia') ?: 'n/a',//olva
