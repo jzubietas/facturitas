035eaba9a5b7f151f05a2a5f87a131c819375be8
Junior Pezantes <werras04@gmail.com>
2023-02-25 10:24:06 -0500

    prueba 1

--- resources/views/layouts/modal/modal1.blade.php
+++ resources/views/layouts/modal/modal1.blade.php
@@ -10,25 +10,10 @@
 
                     <input type="hidden" class="d-none" id="courierregistro2" name="courierregistro2">
 
-                    <div class="form-row">
-                        <div class="form-group col-lg-6">
-                            {!! Form::label('opciones_modal1', 'Opciones') !!}
-                            <select name="opciones_modal1" class="border form-control  border-secondary selectpicker" id="opciones_modal1"
-                                    data-live-search="true" title="Seleccione">
-                            </select>
-                        </div>
-                    </div>
 
                     <div id="op-1-row" class="op-1-row"><!---->
                         <form id="form-op-1-row" name="form-op-1-row" class="form-row ">
-                            <div class="form-group col-lg-6">
-                                {!! Form::label('asesor_op1', 'Asesor*') !!}
 
-                                <select name="asesor_op1" class="border form-control border-secondary" id="asesor_op1"
-                                    data-ruta="{{route('cargar.clientemodal1')}}">
-                                    <option value="">---- SELECCIONE ASESOR ----</option>
-                                </select>
-                            </div>
                             <div class="form-group col-lg-6">
                                 {!! Form::label('cliente_op1', 'Cliente*') !!}
                                 <select name="cliente_op1" class="border form-control border-secondary" id="cliente_op1" data-live-search="true" >
@@ -60,13 +45,7 @@
 
                     <div id="op-2-row" class="op-2-row"><!---->
                         <form id="form-op-2-row" name="form-op-2-row" class="form-row ">
-                            <div class="form-group col-lg-6">
-                                {!! Form::label('asesor_op2', 'Asesor*') !!}
-                                <select name="asesor_op2" class="border form-control border-secondary" id="asesor_op2" data-live-search="true"
-                                        data-ruta="{{route('cargar.clientemodal1')}}">
-                                    <option value="">---- SELECCIONE ASESOR ----</option>
-                                </select>
-                            </div>
+
                             <div class="form-group col-lg-6">
                                 {!! Form::label('cliente_op2', 'Cliente*') !!}
                                 <select name="cliente_op2" class="border form-control border-secondary" id="cliente_op2" data-live-search="true" >
@@ -93,13 +72,7 @@
 
                     <div id="op-3-row" class="op-3-row"><!---->
                         <form id="form-op-3-row" name="form-op-3-row" class="form-row ">
-                            <div class="form-group col-lg-6">
-                                {!! Form::label('asesor_op3', 'Asesor*') !!}
-                                <select name="asesor_op3" class="border form-control border-secondary" id="asesor_op3" data-live-search="true"
-                                        data-ruta="{{route('cargar.clientemodal1')}}">
-                                    <option value="">---- SELECCIONE ASESOR ----</option>
-                                </select>
-                            </div>
+
                             <div class="form-group col-lg-6">
                                 {!! Form::label('cliente_op3', 'Cliente*') !!}
                                 <select name="cliente_op3" class="border form-control border-secondary" id="cliente_op3" data-live-search="true" >
@@ -127,15 +100,6 @@
                     <div id="op-4-row" class="op-4-row d-none">
                         <form id="form-op-4-row" name="form-op-4-row" class="form-row ">
                             <input type="hidden" id="opcion4" name="opcion" value="4">
-
-                            <div class="form-group col-lg-6">
-                                {!! Form::label('asesor_op4', 'Asesor*') !!}
-                                <select name="asesor_op4" class="border form-control border-secondary" id="asesor_op4"
-                                        data-live-search="true"
-                                        data-ruta="{{route('cargar.clientemodal1')}}">
-                                    <option value="">---- SELECCIONE ASESOR ----</option>
-                                </select>
-                            </div>
                             <div class="form-group col-lg-6">
                                 {!! Form::label('cliente_op4', 'Cliente*') !!}
                                 <select name="cliente_op4" class="border form-control border-secondary" id="cliente_op4" data-live-search="true" >
