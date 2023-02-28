3849414ff60ab89a52caafa47a5771ff0e77cae5
Junior Pezantes <werras04@gmail.com>
2023-02-28 17:10:33 -0500

    correccion ruta masiva

--- resources/views/envios/recepcionMotorizado.blade.php
+++ resources/views/envios/recepcionMotorizado.blade.php
@@ -1122,8 +1122,7 @@
                                 url: "{{ route('envios.recepcionmotorizado.iniciar_ruta_masiva') }}",
                             }).always(function (data) {
                                 console.log(data);
-                                $('#tablaPrincipal').DataTable().ajax.reload();
-                                /*if(data.html=='1')
+                                if(data.html=='1')
                                 {
                                   $('#tablaPrincipal').DataTable().ajax.reload();
                                 }else{
@@ -1132,13 +1131,13 @@
                                       'No tiene el rol suficiente para esta operacion',
                                       'error'
                                   )
-                                }*/
+                                }
                                 //self.close()
                                 //self.hideLoading(true)
 
                             });
 
-                        })
+                        });
 
                         /************
                          * ESCANEAR PEDIDO
