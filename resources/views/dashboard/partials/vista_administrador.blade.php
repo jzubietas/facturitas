f81ba60218cb95077bcecbd342b452771f0768a7
Junior Pezantes <werras04@gmail.com>
2023-02-28 15:36:23 -0500

    eliminar fecha

--- resources/views/dashboard/partials/vista_administrador.blade.php
+++ resources/views/dashboard/partials/vista_administrador.blade.php
@@ -94,8 +94,8 @@
         <h1 class="text-uppercase justify-center text-center">Metas del mes</h1>
         <button style="background: none; border: none" onclick="openFullscreen();"><i class="fas fa-expand-arrows-alt ml-3" style="font-size: 20px"></i></button>
         <div class="d-flex justify-content-center align-items-center ml-5">
+          {{--
           <label class="p-0 m-0" for="ingresar">Fecha: </label>
-{{--
           <input type="date" id="calendario" class="border-0 ml-3" min="{{\Carbon\Carbon::now()->startOfDay()->startOfMonth()->format('Y-m-d')}}" max="{{\Carbon\Carbon::now()->endOfDay()->format('Y-m-d')}}">
 --}}
         </div>
@@ -414,11 +414,4 @@
         }
       });
     </script>
-
-  <script>
-    $("#calendario").click(function(e){
-        var data = $('#calendario').val();
-        console.log(data)
-    });
-  </script>
 @endsection
