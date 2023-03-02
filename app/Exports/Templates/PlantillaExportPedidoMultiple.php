8f8476ab1c56598da5ff9c5f28d8791dace54541
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-03-02 08:40:49 -0500

    reporte general clientes

--- app/Exports/Templates/PlantillaExportPedidoMultiple.php
+++ app/Exports/Templates/PlantillaExportPedidoMultiple.php
@@ -6,6 +6,7 @@ namespace App\Exports\Templates;
 use App\Exports\Templates\Sheets\PageclienteinfoDiciembre;
 use App\Exports\Templates\Sheets\PageclienteinfoEnero2023;
 use App\Exports\Templates\Sheets\PageclienteinfoFebrero2023;
+use App\Exports\Templates\Sheets\PageclienteinfoMarzo2023;
 use App\Exports\Templates\Sheets\PageclienteinfoNoviembre;
 use App\Exports\Templates\Sheets\PageclienteSituacion;
 use App\Exports\Templates\Sheets\PagepedidosInfo;
@@ -26,10 +27,11 @@ class PlantillaExportPedidoMultiple implements WithMultipleSheets
     {
         return [
           new PageclienteSituacion(),
-           new PageclienteinfoNoviembre(),
-            new PageclienteinfoDiciembre(),
-            new PageclienteinfoEnero2023(),
-            new PageclienteinfoFebrero2023(),
+          new PageclienteinfoNoviembre(),
+          new PageclienteinfoDiciembre(),
+          new PageclienteinfoEnero2023(),
+          new PageclienteinfoFebrero2023(),
+          new PageclienteinfoMarzo2023(),
         ];
     }
 }
--- app/Exports/Templates/Sheets/PageclienteinfoMarzo2023.php
+++ app/Exports/Templates/Sheets/PageclienteinfoMarzo2023.php
@@ -12,11 +12,11 @@ use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 use Maatwebsite\Excel\Concerns\WithColumnWidths;
 
-class PageclienteinfoNoviembre extends Export implements WithColumnFormatting,WithColumnWidths
+class PageclienteinfoMarzo2023 extends Export implements WithColumnFormatting,WithColumnWidths
 {
     public function collection()
     {
-        $cliente= Cliente::with('user')
+        $cliente = Cliente::with('user')
             ->join('users as u', 'clientes.user_id', 'u.id')
             ->select(
                 'clientes.id'
@@ -25,9 +25,8 @@ class PageclienteinfoNoviembre extends Export implements WithColumnFormatting,Wi
                 ,'clientes.dni'
                 ,'clientes.icelular'
                 ,'clientes.celular'
-                ,DB::raw(" (select a.situacion from situacion_clientes a where a.cliente_id=clientes.id and a.periodo='2022-11') as situacion ")
-                //,DB::raw(" (select a.s_2022_11 from listado_resultados a where a.id=clientes.id ) as situacion ")
-                ,DB::raw("(select DATE_FORMAT(dp1.created_at,'%Y-%m-%d %h:%i:%s') from pedidos dp1 where dp1.estado=1 and dp1.cliente_id=clientes.id order by dp1.created_at desc limit 1) as fecha"),
+                ,'clientes.situacion'
+                ,DB::raw("(select DATE_FORMAT(dp1.created_at,'%Y-%m-%d %h:%i:%s') from pedidos dp1 where dp1.estado=1 and  dp1.cliente_id=clientes.id order by dp1.created_at desc limit 1) as fecha"),
             )
             ->where('clientes.estado', '1')
             ->where('clientes.tipo', '1');
@@ -59,12 +58,11 @@ class PageclienteinfoNoviembre extends Export implements WithColumnFormatting,Wi
             ,"fecha"=>"Fecha Ultimo Pedido"
         ];
     }
+
     public function title(): string
     {
-        //return parent::title();//Por defecto se toma del nombre de la clase de php, en este caso seria "Pagina One" de titulo
-        return 'Detalle Noviembre';
+        return 'Detalle Febrero 2023';
     }
-
     public function map($model): array
     {
         $model->Periodo=strval(str_pad($model->Periodo,2,"0"));//->setDataType(DataType::TYPE_STRING);
