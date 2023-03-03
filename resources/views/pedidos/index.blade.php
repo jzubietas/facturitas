9983bacde1c44e3d8d3218139d3743aac38db5c5
Junior Pezantes <werras04@gmail.com>
2023-03-02 19:02:24 -0500

    color de columna

--- resources/views/pedidos/index.blade.php
+++ resources/views/pedidos/index.blade.php
@@ -1203,15 +1203,15 @@
           var pedidodiferencia = data.diferencia;
 
           if (data.condicion_code == 4 || data.estado == 0) {
-            $('td:eq(12)', row).css('background', '#ff7400').css('color', '#ffffff').css('text-align', 'center').css('font-weight', 'bold');
+            $('td:eq(11)', row).css('background', '#ff7400').css('color', '#ffffff').css('text-align', 'center').css('font-weight', 'bold');
           } else {
             if (pedidodiferencia == null) {
-              $('td:eq(12)', row).css('background', '#ca3a3a').css('color', '#ffffff').css('text-align', 'center').css('font-weight', 'bold');
+              $('td:eq(11)', row).css('background', '#ca3a3a').css('color', '#ffffff').css('text-align', 'center').css('font-weight', 'bold');
             } else {
               if (pedidodiferencia > 3) {
-                $('td:eq(12)', row).css('background', '#ca3a3a').css('color', '#ffffff').css('text-align', 'center').css('font-weight', 'bold');
+                $('td:eq(11)', row).css('background', '#ca3a3a').css('color', '#ffffff').css('text-align', 'center').css('font-weight', 'bold');
               } else {
-                $('td:eq(12)', row).css('background', '#44c24b').css('text-align', 'center').css('font-weight', 'bold');
+                $('td:eq(11)', row).css('background', '#44c24b').css('text-align', 'center').css('font-weight', 'bold');
               }
             }
           }
