6b4de02004c33795900b9daf1d073f94da87ce97
Junior Pezantes <werras04@gmail.com>
2023-03-04 13:01:25 -0500

    Correcion en fullscreen

--- resources/views/dashboard/partials/vista_administrador.blade.php
+++ resources/views/dashboard/partials/vista_administrador.blade.php
@@ -77,7 +77,6 @@
   @include('dashboard.partials.vista_quitar_vidas')
 </div>
 
-<div class="row">
 
   <div class="col-md-12">
     <x-tabla-list-llamada-atencion></x-tabla-list-llamada-atencion>
@@ -92,8 +91,8 @@
 
   <div class="col-lg-12 " id="contenedor-fullscreen">
 
-    <div class="d-flex justify-content-center flex-wrap">
-      <div class="d-flex">
+    <div class="d-flex justify-content-center flex-column flex-wrap justify-content-center">
+      <div class="d-flex justify-content-center">
         <h1 class="text-uppercase justify-center text-center">Metas del mes</h1>
         <button style="background: none; border: none" onclick="openFullscreen();"><i
             class="fas fa-expand-arrows-alt ml-3" style="font-size: 20px"></i></button>
@@ -120,7 +119,7 @@
               <div id="supervisor_B"></div>
             </div>
             <div class="col-md-12">
-              <div id="metas_total" ></div>
+              <div id="metas_total"></div>
             </div>
 
           </div>
@@ -128,173 +127,166 @@
         </div>
       </div>
       {{--FIN-TABLA-DUAL--}}
-    </div>
-
-    <br>
-
-    <div class="container-fluid">
-      <div class="col-md-12">
-        <div class="d-flex justify-content-center">
-          <h1 class="text-uppercase justify-center text-center">Metas Asesores de Llamadas</h1>
-        </div>
-        <div id="metas_situacion_clientes"></div>
       </div>
+
+  </div>
+<br>
+<div class="container-fluid">
+  <div class="col-md-12">
+    <div class="d-flex justify-content-center">
+      <h1 class="text-uppercase justify-center text-center">Metas Asesores de Llamadas</h1>
     </div>
-
-    <div class="container-fluid">
-      <div class="col-md-12">
-        <div class="card bg-cyan">
-          <div class="card-header">
-            <h1 class="text-uppercase justify-center text-center">Metas Cobranzas</h1>
-          </div>
-          <div class="card-body">
-            <div id="metas_cobranzas_general"></div>
-          </div>
-          <div class="card-fotter"></div>
-        </div>
-
+    <div id="metas_situacion_clientes"></div>
+  </div>
+</div>
+{{--METAS COBRANZA--}}
+<div class="container-fluid">
+  <div class="col-md-12">
+    <div class="card bg-cyan">
+      <div class="card-header">
+        <h1 class="text-uppercase justify-center text-center">Metas Cobranzas</h1>
       </div>
+      <div class="card-body">
+        <div id="metas_cobranzas_general"></div>
+      </div>
+      <div class="card-fotter"></div>
     </div>
-    {{--    <div class="container-fluid">--}}
-    {{--      <div class="row">--}}
-    {{--        <div class="col-md-12">--}}
-    {{--          <div id="reporteanalisis"></div>--}}
-    {{--        </div>--}}
-    {{--      </div>--}}
-    {{--    </div>--}}
 
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card">
-            <div class="d-flex justify-content-end align-items-center">
-              <div class="card my-2 mx-2">
-                @php
-                  try {
-                       $currentDate=\Carbon\Carbon::createFromFormat('m-Y',request('selected_date',now()->format('m-Y')));
-                  }catch (Exception $ex){
-                       $currentDate=\Carbon\Carbon::createFromFormat('m-Y',now()->format('m-Y'));
-                  }
+  </div>
+</div>
+{{--HISTORIAL DEL MES PASADO--}}
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="d-flex justify-content-end align-items-center">
+          <div class="card my-2 mx-2">
+            @php
+              try {
+                   $currentDate=\Carbon\Carbon::createFromFormat('m-Y',request('selected_date',now()->format('m-Y')));
+              }catch (Exception $ex){
+                   $currentDate=\Carbon\Carbon::createFromFormat('m-Y',now()->format('m-Y'));
+              }
 
-                @endphp
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"> Seleccionar Mes</span>
-                  </div>
-                  <div class="input-group-prepend">
-                    <a
-                      href="{{route('dashboard.index',['selected_date'=>$currentDate->clone()->startOfMonth()->subYear()->format('m-Y')])}}"
-                      class="btn m-0 p-0"
-                      data-toggle="tooltip" data-placement="top" title="Un año atras">
+            @endphp
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text"> Seleccionar Mes</span>
+              </div>
+              <div class="input-group-prepend">
+                <a
+                  href="{{route('dashboard.index',['selected_date'=>$currentDate->clone()->startOfMonth()->subYear()->format('m-Y')])}}"
+                  class="btn m-0 p-0"
+                  data-toggle="tooltip" data-placement="top" title="Un año atras">
                             <span class="input-group-text">
                                 <
                             </span>
-                    </a>
-                    <a
-                      href="{{route('dashboard.index',['selected_date'=>$currentDate->clone()->startOfMonth()->subMonth()->format('m-Y')])}}"
-                      class="btn m-0 p-0"
-                      data-toggle="tooltip" data-placement="top" title="Un mes atras">
-                      <span class="input-group-text"><</span>
-                    </a>
-                  </div>
-                  <select class="form-control" id="datepickerDashborad"
-                          aria-describedby="basic-addon3">
-                    @foreach([1,2,3,4,5,6,7,8,9,10,11,12] as $month)
-                      @php
-                        $currentMonth=$currentDate->clone()->startOfYear()->addMonths($month-1);
-                      @endphp
-                      <option
-                        {{$currentMonth->format('m-Y')==request('selected_date',now()->format('m-Y'))?'selected':''}}
-                        value="{{$currentMonth->format('m-Y')}}"
-                      >{{Str::ucfirst($currentMonth->monthName)}} {{$currentMonth->year}}</option>
-                    @endforeach
-                  </select>
-
-                  <div class="input-group-append">
-                    <a
-                      href="{{route('dashboard.index',['selected_date'=>$currentDate->clone()->addMonths()->format('m-Y')])}}"
-                      class="btn m-0 p-0"
-                      data-toggle="tooltip" data-placement="top" title="Un mes adelante">
-                      <span class="input-group-text">></span>
-                    </a>
-                  </div>
-                  <div class="input-group-append">
-                    <a
-                      href="{{route('dashboard.index',['selected_date'=>$currentDate->clone()->addYear()->format('m-Y')])}}"
-                      class="btn m-0 p-0"
-                      data-toggle="tooltip" data-placement="top" title="Un año adelante">
-                      <span class="input-group-text">></span>
-                    </a>
-                  </div>
-                </div>
+                </a>
+                <a
+                  href="{{route('dashboard.index',['selected_date'=>$currentDate->clone()->startOfMonth()->subMonth()->format('m-Y')])}}"
+                  class="btn m-0 p-0"
+                  data-toggle="tooltip" data-placement="top" title="Un mes atras">
+                  <span class="input-group-text"><</span>
+                </a>
               </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-12">
-          <div class="row" id="widget-container">
-            <div class="col-md-12">
-              <div class="card">
-                <div class="card-body">
-                  <div class="mb-4 pb-4">
-                    <ul class="list-group">
-                      <li class="list-group-item">
-                        <div class="row">
-                          <div class="col-md-9">
-                            {{--<x-grafico-meta-pedidos-progress-bar></x-grafico-meta-pedidos-progress-bar>--}}
-                            {{--<x-grafico-cobranzas-meses-progressbar></x-grafico-cobranzas-meses-progressbar>--}}
-                          </div>
-                          <div class="col-md-3">
-                            {{--<x-grafico-pedidos-mes-count-progress-bar></x-grafico-pedidos-mes-count-progress-bar>--}}
-                          </div>
-                        </div>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-12">
-              <div class="row">
-                <div class="col-md-6">
-                  {{--<x-grafico-pedidos-atendidos-anulados></x-grafico-pedidos-atendidos-anulados>--}}
-                </div>
+              <select class="form-control" id="datepickerDashborad"
+                      aria-describedby="basic-addon3">
+                @foreach([1,2,3,4,5,6,7,8,9,10,11,12] as $month)
+                  @php
+                    $currentMonth=$currentDate->clone()->startOfYear()->addMonths($month-1);
+                  @endphp
+                  <option
+                    {{$currentMonth->format('m-Y')==request('selected_date',now()->format('m-Y'))?'selected':''}}
+                    value="{{$currentMonth->format('m-Y')}}"
+                  >{{Str::ucfirst($currentMonth->monthName)}} {{$currentMonth->year}}</option>
+                @endforeach
+              </select>
 
-                <div class="col-lg-12">
-                  {{--<x-grafico-pedido_cobranzas-del-dia></x-grafico-pedido_cobranzas-del-dia>--}}
-                </div>
+              <div class="input-group-append">
+                <a
+                  href="{{route('dashboard.index',['selected_date'=>$currentDate->clone()->addMonths()->format('m-Y')])}}"
+                  class="btn m-0 p-0"
+                  data-toggle="tooltip" data-placement="top" title="Un mes adelante">
+                  <span class="input-group-text">></span>
+                </a>
               </div>
-            </div>
-            <div class="col-md-12">
-              {{--<x-grafico-pedidos-por-dia rol="Administrador"
-                                         title="Cantidad de pedidos de los asesores por dia"
-                                         label-x="Asesores" label-y="Cant. Pedidos"
-                                         only-day></x-grafico-pedidos-por-dia>--}}
-
-              {{--<x-grafico-pedidos-por-dia rol="Administrador"
-                                         title="Cantidad de pedidos de los asesores por mes"
-                                         label-x="Asesores"
-                                         label-y="Cant. Pedidos"></x-grafico-pedidos-por-dia>--}}
+              <div class="input-group-append">
+                <a
+                  href="{{route('dashboard.index',['selected_date'=>$currentDate->clone()->addYear()->format('m-Y')])}}"
+                  class="btn m-0 p-0"
+                  data-toggle="tooltip" data-placement="top" title="Un año adelante">
+                  <span class="input-group-text">></span>
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-          {{--<x-grafico-top-clientes-pedidos top="10"></x-grafico-top-clientes-pedidos>--}}
-        </div>
-        <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 d-none">
+    <div class="col-md-12">
+      <div class="row" id="widget-container">
+        <div class="col-md-12">
           <div class="card">
-            <div class="card-body pl-0">
-              {{--<div id="pagosxmes" class="w-100" style="height: 550px;"></div>--}}
+            <div class="card-body">
+              <div class="mb-4 pb-4">
+                <ul class="list-group">
+                  <li class="list-group-item">
+                    <div class="row">
+                      <div class="col-md-9">
+                        {{--<x-grafico-meta-pedidos-progress-bar></x-grafico-meta-pedidos-progress-bar>--}}
+                        {{--<x-grafico-cobranzas-meses-progressbar></x-grafico-cobranzas-meses-progressbar>--}}
+                      </div>
+                      <div class="col-md-3">
+                        {{--<x-grafico-pedidos-mes-count-progress-bar></x-grafico-pedidos-mes-count-progress-bar>--}}
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
+        </div>
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-6">
+              {{--<x-grafico-pedidos-atendidos-anulados></x-grafico-pedidos-atendidos-anulados>--}}
+            </div>
+
+            <div class="col-lg-12">
+              {{--<x-grafico-pedido_cobranzas-del-dia></x-grafico-pedido_cobranzas-del-dia>--}}
+            </div>
+          </div>
+        </div>
+        <div class="col-md-12">
+          {{--<x-grafico-pedidos-por-dia rol="Administrador"
+                                     title="Cantidad de pedidos de los asesores por dia"
+                                     label-x="Asesores" label-y="Cant. Pedidos"
+                                     only-day></x-grafico-pedidos-por-dia>--}}
+
+          {{--<x-grafico-pedidos-por-dia rol="Administrador"
+                                     title="Cantidad de pedidos de los asesores por mes"
+                                     label-x="Asesores"
+                                     label-y="Cant. Pedidos"></x-grafico-pedidos-por-dia>--}}
         </div>
       </div>
     </div>
   </div>
+</div>
+<br>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+      {{--<x-grafico-top-clientes-pedidos top="10"></x-grafico-top-clientes-pedidos>--}}
+    </div>
+    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 d-none">
+      <div class="card">
+        <div class="card-body pl-0">
+          {{--<div id="pagosxmes" class="w-100" style="height: 550px;"></div>--}}
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 
   @push('css')
@@ -442,7 +434,7 @@
 
         $('#fecha').val("{{\Carbon\Carbon::parse($fecha)->format('Y-m-d')}}");
 
-        $(document).on('change','#fecha',function(){
+        $(document).on('change', '#fecha', function () {
           const value = e.target.value;
           console.log(value)
           if (value) {
@@ -451,28 +443,26 @@
         });
 
         window.cargaNueva = function (entero) {
-          console.log(' '+entero)
-          var fd=new FormData();
-          fd.append('fecha',$('#fecha').val());
-          fd.append('ii',entero);
+          console.log(' ' + entero)
+          var fd = new FormData();
+          fd.append('fecha', $('#fecha').val());
+          fd.append('ii', entero);
           $.ajax({
             data: fd,
             processData: false,
             contentType: false,
             method: 'POST',
             url: "{{ route('dashboard.viewMetaTable') }}",
-            success: function (resultado){
-              if(entero==1)
-              {
+            success: function (resultado) {
+              if (entero == 1) {
                 $('#metas_dp').html(resultado);
-              }else if(entero==2){
+              } else if (entero == 2) {
                 $('#meta').html(resultado);
-              }
-              else if(entero==3){
+              } else if (entero == 3) {
                 $('#metas_total').html(resultado);
-              }else if(entero==4){
+              } else if (entero == 4) {
                 $('#supervisor_total').html(resultado);
-              }else if(entero==5){
+              } else if (entero == 5) {
                 $('#supervisor_A').html(resultado);
               }
             }
@@ -481,42 +471,42 @@
 
 
         window.cargReporteAnalisis = function () {
-          var fd=new FormData();
+          var fd = new FormData();
           $.ajax({
             data: fd,
             processData: false,
             contentType: false,
             method: 'POST',
             url: "{{ route('dashboard.viewAnalisis') }}",
-            success: function (resultado){
+            success: function (resultado) {
               $('#reporteanalisis').html(resultado);
             }
           })
         }
 
         window.cargReporteMetasSituacionClientes = function () {
-          var fd=new FormData();
+          var fd = new FormData();
           $.ajax({
             data: fd,
             processData: false,
             contentType: false,
             method: 'POST',
             url: "{{ route('dashboard.graficoSituacionClientes') }}",
-            success: function (resultado){
+            success: function (resultado) {
               $('#metas_situacion_clientes').html(resultado);
             }
           })
         }
 
         window.cargReporteMetasCobranzasGeneral = function () {
-          var fd=new FormData();
+          var fd = new FormData();
           $.ajax({
             data: fd,
             processData: false,
             contentType: false,
             method: 'POST',
             url: "{{ route('dashboard.graficoCobranzasGeneral') }}",
-            success: function (resultado){
+            success: function (resultado) {
               $('#metas_cobranzas_general').html(resultado);
             }
           })
@@ -541,12 +531,12 @@
           cargaNueva(5);
         }
 
-        $('a[href$="#myModal"]').on( "click", function() {
+        $('a[href$="#myModal"]').on("click", function () {
           $('#myModal').modal();
         });
 
         var elem = document.querySelector("#contenedor-fullscreen");
-        window.openFullscreen =function () {
+        window.openFullscreen = function () {
           if (elem.requestFullscreen) {
             elem.requestFullscreen();
           } else if (elem.webkitRequestFullscreen) { /* Safari */
