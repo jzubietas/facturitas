19bb24ee3a65abe3078148a599c9a99fce9733e2
agape14 <adelacruzcarlos@gmail.com>
2023-03-03 22:01:12 -0500

    primera parte del listado de clientes
    primera parte del listado de clientes

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Cliente;
 use App\Models\CuentaBancaria;
+use App\Models\DetalleContactos;
 use App\Models\DetallePedido;
 use App\Models\DireccionEnvio;
 use App\Models\PagoPedido;
@@ -1974,4 +1975,138 @@ class ClienteController extends Controller
             "html" => array('status' => $status, 'data' => $data)
         ]);
     }
+
+  public function listtablecontactos(Request $request){ //rbnvalue
+    $data = DetalleContactos::join('clientes as c', 'detalle_contactos.codigo_cliente', 'c.id')
+      ->join('users as u', 'c.user_id', 'u.id')
+      ->where('tipo_insert',$request->tipo)
+      ->orderByRaw("guardado DESC, confirmado DESC")
+      ->select(['detalle_contactos.*']);
+
+
+
+    if (in_array(auth()->user()->rol, [User::ROL_ADMIN, User::ROL_JEFE_LLAMADAS,User::ROL_ENCARGADO])) {
+      if ($request->rbnvalue==1){
+        $data = $data->where('guardado',0)
+          ->where('confirmado',0);
+      }elseif ($request->rbnvalue==2){
+        $data = $data->where('guardado',1)
+          ->where('confirmado',0);
+      }elseif ($request->rbnvalue==3){
+        $data = $data->where('confirmado',0)
+          ->where('tipo_insert',$request->tipo)
+          ->where('tipo_insert',$request->tipo);
+      }else{
+        $data = $data->whereIn('guardado', [0, 1]);
+      }
+    }else if (in_array(auth()->user()->rol, [User::ROL_LLAMADAS,User::ROL_MOTORIZADO])) {
+      if ($request->rbnvalue==1){
+        $data = $data->where('guardado',0)
+          ->where('confirmado',0);
+      }elseif ($request->rbnvalue==2){
+        $data = $data->where('guardado',1)
+          ->where('confirmado',0);
+      }elseif ($request->rbnvalue==3){
+        $data = $data->where('confirmado',0)
+          ->where('tipo_insert',$request->tipo)
+          ->where('tipo_insert',$request->tipo);
+      }else{
+        $data = $data->where('guardado',0);
+      }
+    }else{
+      $data = $data->where('guardado',0);
+    }
+
+    if (Auth::user()->rol == "Llamadas") {
+      $usersasesores = User::where('users.rol', 'Asesor')
+        ->where('users.estado', '1')
+        ->where('users.llamada', Auth::user()->id)
+        ->select(
+          DB::raw("users.identificador as identificador")
+        )
+        ->pluck('users.identificador');
+      $data = $data->WhereIn("u.identificador", $usersasesores);
+
+
+    } else if (Auth::user()->rol == "Jefe de llamadas") {
+      /*$usersasesores = User::where('users.rol', 'Asesor')
+          ->where('users.estado', '1')
+          ->where('users.llamada', Auth::user()->id)
+          ->select(
+              DB::raw("users.identificador as identificador")
+          )
+          ->pluck('users.identificador');
+
+      $data = $data->WhereIn("u.identificador", $usersasesores);*/
+    } elseif (Auth::user()->rol == "Asesor") {
+      $usersasesores = User::where('users.rol', 'Asesor')
+        ->where('users.estado', '1')
+        ->where('users.identificador', Auth::user()->identificador)
+        ->select(
+          DB::raw("users.identificador as identificador")
+        )
+        ->pluck('users.identificador');
+      $data = $data->WhereIn("u.identificador", $usersasesores);
+
+    } else if (Auth::user()->rol == "Encargado") {
+      $usersasesores = User::where('users.rol', 'Asesor')
+        ->where('users.estado', '1')
+        ->where('users.supervisor', Auth::user()->id)
+        ->select(
+          DB::raw("users.identificador as identificador")
+        )
+        ->pluck('users.identificador');
+
+      $data = $data->WhereIn("u.identificador", $usersasesores);
+    } else if (Auth::user()->rol == User::ROL_ASESOR_ADMINISTRATIVO) {
+      //$asesorB=User::activo()->where('identificador','=','B')->pluck('id')
+      $data = $data->Where("u.identificador", '=', 'B');
+    }
+
+    return \Yajra\DataTables\DataTables::of(($data))
+      ->addIndexColumn()
+      ->addColumn('tipo_insert', function ($data) {
+        $vinsert="";;
+        if ($data->tipo_insert==1){
+          $vinsert="NUEVO";
+        } elseif ($data->tipo_insert==2){
+          $vinsert="CAMBIO NOMBRE";
+        }elseif ($data->tipo_insert==3){
+          $vinsert="BLOQUEO";
+        }elseif ($data->tipo_insert==4){
+          $vinsert="CAMBIO NUMERO";
+        }
+        return $vinsert;
+      })
+      ->addColumn('action', function ($data) {
+        $btn = [];
+        $deshabilitar_guardado="";
+        $deshabilitar_confirmado="";
+        if ($data->guardado==0){
+          $deshabilitar_guardado="enabled ";
+          $deshabilitar_confirmado="disabled";
+        }else if ($data->guardado==1 && $data->confirmado==0){
+          $deshabilitar_guardado="disabled";
+          $deshabilitar_confirmado="enabled";
+        }
+        $btn[] = '<div><ul class="m-0 p-1" aria-labelledby="dropdownMenuButton" style="display: flex; grid-gap: 2px;">';
+
+        if (in_array(auth()->user()->rol, [User::ROL_ADMIN, User::ROL_JEFE_LLAMADAS])) {
+          $btn[] = '<button style="font-size:18px" class="m-0 p-2 btn btn-sm btn-success dropdown-item text-break text-wrap btnGuardado" '.$deshabilitar_guardado.'><i class="fa fa-save text-success mr-8"></i></button>';
+          $btn[] = '<button style="font-size:18px" class="m-0 p-2 btn btn-sm btn-danger dropdown-item text-break text-wrap btnConfirmado" '.$deshabilitar_confirmado.'><i class="fa fa-check danger mr-8"></i></button>';
+          $btn[] = '<button style="font-size:18px" class="m-0 p-2 btn btn-sm btn-danger dropdown-item text-break text-wrap btnConfirmado" '.$deshabilitar_confirmado.'><i class="fa fa-check-double danger mr-8"></i></button>';
+        }else if (in_array(auth()->user()->rol, [User::ROL_LLAMADAS])) {
+          $btn[] = '<button style="font-size:18px" class="m-0 p-2 btn btn-sm btn-success dropdown-item text-break text-wrap btnGuardado" '.$deshabilitar_guardado.'><i class="fa fa-save text-success mr-8"></i></button>';
+        }else if (in_array(auth()->user()->rol, [User::ROL_LLAMADAS])) {
+          $btn[] = '<button style="font-size:18px" class="m-0 p-2 btn btn-sm btn-success dropdown-item text-break text-wrap btnGuardado" '.$deshabilitar_guardado.'><i class="fa fa-save text-success mr-8"></i></button>';
+        }
+
+
+        $btn[] = '</ul></div>';
+        return join('', $btn);
+      })
+      ->rawColumns(['action'])
+      ->make(true);
+    //return datatables($detallecontactos)->toJson();
+  }
 }
--- resources/views/layouts/modal/modal2.blade.php
+++ resources/views/layouts/modal/modal2.blade.php
@@ -7,21 +7,108 @@
         <button class="float-right btn btn-danger" data-dismiss="modal"><i class="fas fa-times" aria-hidden="true" ></i></button>
       </div>
       <div class="modal-body">
+        <div class="form-row">
+          <div class="form-group col-lg-12">
 
-        <div id="op-1-row" class="op-1-row"><!---->
-          <table id="tablaListadoLlamadas" class="table table-striped w-100" style="text-align: center">
-            <thead></h4>
-            <tr>
-              <th scope="col">Asesor</th>
-              <th scope="col">Celular</th>
-              <th scope="col">Nombre Cliente</th>
-              <th scope="col">Contacto</th>
-              <th scope="col">Accion</th>
-            </tr>
-            </thead>
-            <tbody>
-            </tbody>
-          </table>
+            <div class="btn-group" role="group" aria-label="Basic example">
+              <button id="btnListNuevoCliente" type="button" class="btn rounded btn-info ml-2"> <i class="fa fa-user"></i> Nuevo Cliente</button>
+              <button id="btnListCambioNombre" type="button" class="btn rounded btn-secondary  ml-2"><i class="fa fa-user-lock"></i> Cambio Nombre</button>
+              <button id="btnListBloqueo" type="button" class="btn rounded btn-danger   ml-2"><i class="fa fa-lock"></i>  Bloqueo</button>
+              <button id="btnListCambioNumero" type="button" class="btn rounded btn-warning  ml-2"><i class="fa fa-phone"></i>  Cambio Numero</button>
+            </div>
+          </div>
+        </div>
+
+
+
+        <div id="modal-ListadoClientes" class="modal-ListadoClientes">
+          <div id="op-1-row" class="op-1-row"><!---->
+            <div class="btn-group" role="group" aria-label="Basic example" id="radioBtnDiv">
+              <div class="form-check ">
+                <input class="form-check-input" type="radio" name="rbnTipo" id="rbnGuardar" checked value="1"/>
+                <label class="form-check-label" for="rbnGuardar"> Sin Guardar </label>
+              </div>
+              <div class="form-check ml-4">
+                <input class="form-check-input" type="radio" name="rbnTipo" id="rbnGuardado" value="2"/>
+                <label class="form-check-label" for="rbnGuardado"> Guardado </label>
+              </div>
+              <div class="form-check ml-4">
+                <input class="form-check-input" type="radio" name="rbnTipo" id="rbnConfirmado" value="3"/>
+                <label class="form-check-label" for="rbnConfirmado"> Confirmado </label>
+              </div>
+            </div>
+            <table id="tablaListadoLlamadas" class="table table-striped w-100" style="text-align: center">
+              <thead></h4>
+              <tr>
+                <th scope="col">Tipo</th>
+                <th scope="col">Asesor</th>
+                <th scope="col">Celular</th>
+                <th scope="col">Nombre Cliente</th>
+                <th scope="col">Contacto</th>
+                <th scope="col">Accion</th>
+              </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <br>
+        <div id="modal-CambioNombre" class="modal-CambioNombre">
+          <div id="op-1-row" class="op-1-row"><!---->
+            <table id="tablaCambioNombre" class="table table-striped w-100" style="text-align: center">
+              <thead></h4>
+              <tr>
+                <th scope="col">Tipo</th>
+                <th scope="col">Asesor</th>
+                <th scope="col">Celular</th>
+                <th scope="col">Nombre Cliente</th>
+                <th scope="col">Contacto</th>
+                <th scope="col">Accion</th>
+              </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <div id="modal-BLoqueoCliente" class="modal-BLoqueoCliente">
+          <div id="op-1-row" class="op-1-row"><!---->
+            <table id="tablaBloqueoClientes" class="table table-striped w-100" style="text-align: center">
+              <thead></h4>
+              <tr>
+                <th scope="col">Tipo</th>
+                <th scope="col">Asesor</th>
+                <th scope="col">Celular</th>
+                <th scope="col">Nombre Cliente</th>
+                <th scope="col">Contacto</th>
+                <th scope="col">Accion</th>
+              </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <div id="modal-CambioNumero" class="modal-CambioNumero">
+          <div id="op-1-row" class="op-1-row"><!---->
+            <table id="tablaCambioNumero" class="table table-striped w-100" style="text-align: center">
+              <thead></h4>
+              <tr>
+                <th scope="col">Tipo</th>
+                <th scope="col">Asesor</th>
+                <th scope="col">Celular</th>
+                <th scope="col">Nombre Cliente</th>
+                <th scope="col">Contacto</th>
+                <th scope="col">Accion</th>
+              </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+          </div>
         </div>
 
       </div>
