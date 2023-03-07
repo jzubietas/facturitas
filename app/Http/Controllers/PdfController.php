ac37dee2e5ae8e1316725459fa171cc7d1b2b843
Junior Pezantes <werras04@gmail.com>
2023-03-07 11:08:36 -0500

    Fecha en metas cobranzas

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -336,7 +336,7 @@ class PdfController extends Controller
 
 
 
-      $title_mes_artificio=$mes_artificio->format('F');
+      $title_mes_artificio=$mes_artificio->format('F - Y');
       //$title_mes_artificio=$title_mes_artificio->formatLocalized('%B');
       $html[] = '<tr>';
       $html[] = '<td style="width:20%;" class="text-center">';
