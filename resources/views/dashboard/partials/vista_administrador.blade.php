5ddf7e4d8cc6033493eafc503181ce73f0569b17
Junior Pezantes <werras04@gmail.com>
2023-03-08 15:20:56 -0500

    Grafico sparkline

--- resources/views/dashboard/partials/vista_administrador.blade.php
+++ resources/views/dashboard/partials/vista_administrador.blade.php
@@ -173,6 +173,53 @@
 </div>
 
 {{--SPARKLINE--}}
+<div class="conatiner-fluid">
+    <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="card">
+            <div class="card-header border-0">
+                <div class="d-flex justify-content-between">
+                    <h3 class="card-title">Online Store Visitors</h3>
+                    {{--<a href="javascript:void(0);">View Report</a>--}}
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="d-flex">
+                    <p class="d-flex flex-column">
+                        <span class="text-bold text-lg">820</span>
+                        <span>Visitors Over Time</span>
+                    </p>
+                    <p class="ml-auto d-flex flex-column text-right">
+                        <span class="text-success">
+                            <i class="fas fa-arrow-up"></i> 12.5%
+                        </span>
+                        <span class="text-muted">Since last week</span>
+                    </p>
+                </div>
+
+                <div class="position-relative mb-4">
+                    <div class="chartjs-size-monitor">
+                        <div class="chartjs-size-monitor-expand">
+                            <div class=""></div>
+                        </div>
+                        <div class="chartjs-size-monitor-shrink">
+                            <div class=""></div>
+                        </div>
+                    </div>
+                    <canvas id="visitors-chart" style="display: block; width: 764px; height: 200px;"
+                            class="chartjs-render-monitor" width="764" height="200"></canvas>
+                </div>
+                <div class="d-flex flex-row justify-content-end">
+                    <span class="mr-2">
+                        <i class="fas fa-square text-primary"></i> This Week
+                    </span>
+                    <span>
+                        <i class="fas fa-square text-gray"></i> Last Week
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <br>
@@ -323,5 +370,54 @@
                 }
             }
         });
+    </script>
+
+    <script src="https://adminlte.io/themes/v3/plugins/chart.js/Chart.min.js"></script>
+    <script>
+        $(function () {
+            var ticksStyle = {fontColor: '#495057', fontStyle: 'bold'}
+            var mode = 'index'
+            var intersect = true
+
+            var $visitorsChart = $('#visitors-chart')
+            var visitorsChart = new Chart($visitorsChart, {
+                data: {
+                    labels: ['18th', '20th', '22nd', '24th', '26th', '28th', '30th'],
+                    datasets: [{
+                        type: 'line',
+                        data: [100, 120, 170, 167, 180, 177, 160],
+                        backgroundColor: 'transparent',
+                        borderColor: '#007bff',
+                        pointBorderColor: '#007bff',
+                        pointBackgroundColor: '#007bff',
+                        fill: false
+                    }, {
+                        type: 'line',
+                        data: [60, 80, 70, 67, 80, 77, 100],
+                        backgroundColor: 'tansparent',
+                        borderColor: '#ced4da',
+                        pointBorderColor: '#ced4da',
+                        pointBackgroundColor: '#ced4da',
+                        fill: false
+                    }]
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    tooltips: {mode: mode, intersect: intersect},
+                    hover: {mode: mode, intersect: intersect},
+                    legend: {display: false},
+                    scales: {
+                        yAxes: [{
+                            gridLines: {
+                                display: true,
+                                lineWidth: '4px',
+                                color: 'rgba(0, 0, 0, .2)',
+                                zeroLineColor: 'transparent'
+                            }, ticks: $.extend({beginAtZero: true, suggestedMax: 200}, ticksStyle)
+                        }], xAxes: [{display: true, gridLines: {display: false}, ticks: ticksStyle}]
+                    }
+                }
+            })
+        })
     </script>
 @endsection
