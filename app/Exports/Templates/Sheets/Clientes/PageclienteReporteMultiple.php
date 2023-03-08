8fb40d87522babd2262e7d9d14e0085661d7af54
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-03-08 11:34:55 -0500

    a

--- app/Exports/Templates/Sheets/Clientes/PageclienteReporteMultiple.php
+++ app/Exports/Templates/Sheets/Clientes/PageclienteReporteMultiple.php
@@ -337,7 +337,7 @@ class PageclienteReporteMultiple extends Export implements WithColumnFormatting,
         $style_nulo = array('fill' => array('fillType' => Fill::FILL_SOLID,'startColor' => array('argb' => $color_cabeceras)));
         $style_abandono_reciente = array('fill' => array('fillType' => Fill::FILL_SOLID,'startColor' => array('argb' => $color_cabeceras)));
 
-        $row_cell_=21;
+        $row_cell_=22;
         $letter_cell='W';
         foreach ($event->sheet->getRowIterator() as $row)
         {
