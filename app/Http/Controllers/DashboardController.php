950f56c85ce3913f662a5a418080e3f31a60d14f
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-03-16 17:29:54 -0500

    cargar

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -605,7 +605,7 @@ class DashboardController extends Controller
             $item['meta_new'] = $meta_new;
             $item['porcentaje_general']=($all/$allmeta_2);
             return $item;
-        })->sortBy('progress_pedidos', SORT_NUMERIC, true);//->all();
+        })->sortBy('porcentaje_general', SORT_NUMERIC, true);//->all();
 
         if ($request->ii == 1) {
             if ($total_asesor % 2 == 0) {
