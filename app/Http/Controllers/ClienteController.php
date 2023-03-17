98aa6bd75e799f8e91ee5293e8887907ffa18c45
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-03-17 16:38:27 -0500

    carga

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -780,6 +780,7 @@ class ClienteController extends Controller
                 'clientes.celular',
                 'clientes.nombre',
                 'clientes.saldo',
+                DB::raw("(select count(pesup.id) from pedidos pesup where pesup.cliente_id=clientes.id ) as solo_un_pedido"),
                 DB::raw(" (select count(ped.id) from pedidos ped where ped.cliente_id=clientes.id and ped.pago in (0,1) and ped.pagado in (0,1) and cast(ped.created_at as date) >='" . now()->startOfMonth()->format('Y-m-d') . "' and ped.estado=1) as pedidos_mes_deuda "),
                 DB::raw(" (select count(ped2.id) from pedidos ped2 where ped2.cliente_id=clientes.id and ped2.pago in (0,1) and ped2.pagado in (0,1) and cast(ped2.created_at as date) <='" . now()->startOfMonth()->subMonth()->endOfMonth()->endOfDay()->format('Y-m-d') . "'  and ped2.estado=1) as pedidos_mes_deuda_antes "),
             ]);
