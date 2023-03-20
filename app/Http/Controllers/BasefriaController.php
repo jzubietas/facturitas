d7e5361b3e4c20f135eeedbbc1fab355d3be837b
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-03-20 09:29:26 -0500

    carga

--- app/Http/Controllers/BasefriaController.php
+++ app/Http/Controllers/BasefriaController.php
@@ -166,7 +166,7 @@ class BasefriaController extends Controller
         $usersB = User::where('users.estado', '1')
             ->whereIn('rol', [User::ROL_ASESOR_ADMINISTRATIVO])//51
             ->first();
-        dd($usersB);
+        //dd($usersB);
         $users = collect();
         $users->put($usersB->id, $usersB->identificador);
         $usersall = User::select(
