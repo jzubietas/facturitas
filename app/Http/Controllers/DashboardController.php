11fece2ce82a6c3883a22c781077888d37acee54
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-03-25 10:51:11 -0500

    carga

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -408,7 +408,9 @@ class DashboardController extends Controller
                 ->count();
 
             //recurrente mes pasado
-            $clientes_situacion_recurrente = SituacionClientes::query()->join('users as u', 'u.id', 'situacion_clientes.user_id')
+            $clientes_situacion_recurrente = SituacionClientes::query()
+                ->join('clientes as c', 'c.id', 'situacion_clientes.cliente_id')
+                ->join('users as u', 'u.id', 'c.user_id')
                 ->where('u.id', $asesor->id)
                 ->whereIn('situacion_clientes.situacion',['RECUPERADO ABANDONO','RECUPERADO RECIENTE','NUEVO','ACTIVO'])
                 ->where('situacion_clientes.periodo',Carbon::now()->clone()->subMonth()->format('Y-m'))
