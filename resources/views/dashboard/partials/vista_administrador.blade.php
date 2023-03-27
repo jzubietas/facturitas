7f48615fb8a2b6f38150279abd57801b7e3ece7c
agape14 <adelacruzcarlos@gmail.com>
2023-03-27 10:11:22 -0500

    colocando icono

--- resources/views/dashboard/partials/vista_administrador.blade.php
+++ resources/views/dashboard/partials/vista_administrador.blade.php
@@ -181,7 +181,7 @@ text-shadow: 2px 2px 0 #242120, 2px -2px 0 #242120, -2px 2px 0 #242120, -2px -2p
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12">
             <br><br>
-            <h1 class="text-center">Dejaron de pedir -check</h1>
+            <h1 class="text-center">Dejaron de pedir <i class="fas fa-exclamation-circle"></i> </h1>
         </div>
         <div class="contain-table-dual row" style="width: 100% !important;">
             <div class="col-lg-6" id="grafico_dejaronpedir_left"></div>
