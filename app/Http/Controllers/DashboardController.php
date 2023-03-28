e4d95b239858fe4d90a19cfbbbf8ea3edc3ae67a
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-03-28 18:22:07 -0500

    cargar

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1368,7 +1368,7 @@ class DashboardController extends Controller
                 }
                 else
                 {
-                    $html .= '<div class="progress-bar bg-danger" role="progressbar"
+                    $html .= '<div class="progress-bar bg-info" role="progressbar"
                          style="width: ' . ($round) . '%"
                          aria-valuenow="' . ($round) . '"
                          aria-valuemin="0"
