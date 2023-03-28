409e25d19045e72440986a666e9a9226881d5447
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-03-28 10:41:16 -0500

    carga

--- app/Exports/Templates/Sheets/Envios/PageclienteCuatromeses.php
+++ app/Exports/Templates/Sheets/Envios/PageclienteCuatromeses.php
@@ -65,11 +65,11 @@ class PageclienteCuatromeses extends Export implements WithColumnFormatting,With
                 DB::raw("(select case when dp1.pagado=0 then 'DEUDA'
                                         when dp1.pagado=1 then 'DEUDA'
                                         else 'NO DEUDA' end from pedidos dp1
-                                        where dp1.estado=1 and dp1.cliente_id=clientes.id order by dp1.created_at desc limit 1) as deuda"),
+                                        where dp1.estado=1 and dp1.cliente_id=clientes.id and dp1.codigo not like '%-C%' order by dp1.created_at desc limit 1) as deuda"),
                 DB::raw("(select dp2.saldo from pedidos a inner join detalle_pedidos dp2 on a.id=dp2.pedido_id
-                                        where dp2.estado=1 and a.cliente_id=clientes.id order by dp2.created_at desc limit 1) as importeultimopedido"),
+                                        where dp2.estado=1 and a.cliente_id=clientes.id and a.codigo not like '%-C%' order by dp2.created_at desc limit 1) as importeultimopedido"),
                 DB::raw("(select DATE_FORMAT(dp3.created_at,'%m') from pedidos a inner join detalle_pedidos dp3 on a.id=dp3.pedido_id
-                                        where dp3.estado=1 and a.cliente_id=clientes.id order by dp3.created_at desc limit 1) as mesultimopedido"),
+                                        where dp3.estado=1 and a.cliente_id=clientes.id and a.codigo not like '%-C%' order by dp3.created_at desc limit 1) as mesultimopedido"),
             ]);
 
         if (Auth::user()->rol == User::ROL_LLAMADAS) {
