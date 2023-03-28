1bd451d7f4b4e9c7182b61a0ea69256582a20728
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-03-28 11:03:25 -0500

    cargar

--- app/Console/Commands/AnalisisSituacionCliente.php
+++ app/Console/Commands/AnalisisSituacionCliente.php
@@ -427,7 +427,7 @@ class AnalisisSituacionCliente extends Command
                                                       // es diciembre
                                                       //a abandono
                                                       $situacion_create->update([
-                                                          "situacion" => 'NULO',"flag_fp" => '1'
+                                                          "situacion" => 'RECUPERADO ABANDONO',"flag_fp" => '1'
                                                       ]);
 
                                                   }else if($situacion_antes_3->activos>0)
