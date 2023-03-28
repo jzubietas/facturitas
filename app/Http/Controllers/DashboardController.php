9ef9b22b0283f77575a8ed3ad5698ddb598eb9f8
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-03-28 17:12:48 -0500

    cargar

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4218,7 +4218,7 @@ class DashboardController extends Controller
                 style="top: 3px !important;height: 30px !important;font-size: 12px;">
              <span style="font-weight: lighter"> <b style="font-weight: bold !important; font-size: 16px; text-transform: uppercase;">
              LUIS: TOTAL DEJARON DE PEDIR -  ' . Carbon::parse($fechametames)->monthName . ' : ' . round(($count_asesor[46]['all_situacion_activo'] / $count_asesor[46]['all_situacion_recurrente']) * 100, 2) . '%</b> - '
-                    . $count_asesor[46]['all_situacion_activo'] . '/' . $count_asesor[46]['all_situacion_recurrente'] . '</span>
+                    . $count_asesor[46]['all_situacion_activo'] . '/ Recurrente. ' . $count_asesor[46]['all_situacion_recurrente'].' + Activo. '.$count_asesor[46]['all_situacion_activo'] . '</span>
     </div>';
             }
 
@@ -4335,7 +4335,7 @@ class DashboardController extends Controller
                 style="top: 3px !important;height: 30px !important;font-size: 12px;">
              <span style="font-weight: lighter"> <b style="font-weight: bold !important; font-size: 16px; text-transform: uppercase;">
              PAOLA: TOTAL DEJARON DE PEDIR -  ' . Carbon::parse($fechametames)->monthName . ' : ' . round(($count_asesor[24]['all_situacion_activo'] / ($count_asesor[24]['all_situacion_recurrente']+$count_asesor[24]['all_situacion_activo']) ) * 100, 2) . '%</b> - '
-                    . $count_asesor[24]['all_situacion_activo'] . '/' . ($count_asesor[24]['all_situacion_recurrente'] +$count_asesor[24]['all_situacion_activo']) . '</span>
+                    . $count_asesor[24]['all_situacion_activo'] . '/ Recurrentes. ' . ($count_asesor[24]['all_situacion_recurrente']).' + Activos. '.($count_asesor[24]['all_situacion_activo']) . '</span>
     </div>';
             }
 
