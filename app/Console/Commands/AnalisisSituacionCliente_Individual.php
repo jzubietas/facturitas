8d1e095c631ba34b0322375c8eb050a1e0d15bdf
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-03-29 09:47:22 -0500

    a

--- app/Console/Commands/AnalisisSituacionCliente_Individual.php
+++ app/Console/Commands/AnalisisSituacionCliente_Individual.php
@@ -136,6 +136,7 @@ class AnalisisSituacionCliente_Individual extends Command
                             $situacion_antes=SituacionClientes::where('cliente_id',$cliente->id)
                                 ->where('periodo',$mes_antes->format('Y-m'))->first();
 
+                            $this->warn("mes ejecucion  ->".$periodo_ejecucion );
                             $this->warn($situacion_antes);
 
                             switch($situacion_antes->situacion)
