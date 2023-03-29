e3e3a3f4ee738ecdc758b4fbfa22f1920b594327
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-03-29 12:29:18 -0500

    ca

--- app/Console/Commands/AnalisisSituacionClientePersonalizado.php
+++ app/Console/Commands/AnalisisSituacionClientePersonalizado.php
@@ -59,8 +59,8 @@ class AnalisisSituacionClientePersonalizado extends Command
 
 
     $clientes=Cliente::whereIn('tipo',['0','1'])
-      ->where('situacion','=','RECUPERADO RECIENTE')
-        //->whereNull('situacion')
+      //->where('situacion','=','RECUPERADO RECIENTE')
+        ->whereNull('situacion')
       ->orderBy('id','asc')->get();
 
     $progress = $this->output->createProgressBar($clientes->count());
