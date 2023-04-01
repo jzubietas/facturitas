a2bcf2184a5d6b01ae0c1a9dc000fdb0f9759037
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-04-01 10:48:59 -0500

    cargar

---
+++ database/migrations/2023_04_01_101317_create_table_usermovimientos.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableUsermovimientos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users_movimientos', function (Blueprint $table) {
+            //
+            $table->id();
+            $table->string('accion')->default('')->nullable();
+            $table->string('detalle')->default('')->nullable();
+            $table->integer('user_id')->default(0)->nullable();
+            $table->string('rol')->default('')->nullable();
+            $table->timestamp("created_at")->nullable()->default(now());
+            $table->timestamp("updated_at")->nullable()->default(now());
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users_movimientos', function (Blueprint $table) {
+            //
+        });
+    }
+}
