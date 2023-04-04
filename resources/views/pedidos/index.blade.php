5a538997275db1ab63d514dcd7e8204d04fb9fce
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-04-04 17:29:18 -0500

    carga

--- resources/views/pedidos/index.blade.php
+++ resources/views/pedidos/index.blade.php
@@ -1456,7 +1456,7 @@ ${data.foto3 ? `
                     "infoPostFix": "",
                     "thousands": ",",
                     "lengthMenu": "Mostrar _MENU_ Entradas",
-                    "processing": "Procesando...",
+                    "processing": '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span> ',
                     "search": "Buscar:",
                     "zeroRecords": "Sin resultados encontrados",
                     "paginate": {
