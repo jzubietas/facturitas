03e7d846b0c272e3605016e8c83ecea4bb243765
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-04-04 16:39:48 -0500

    carga

--- resources/views/pedidos/index.blade.php
+++ resources/views/pedidos/index.blade.php
@@ -1476,6 +1476,10 @@ ${data.foto3 ? `
                             }
                           }
                         ],*/
+                "drawCallback":function(settings)
+                {
+                    $(".dataTables_wrapper .dataTables_processing").addClass("processing_custom_datatable_personal");
+                }
             });
 
             function charge_corrections(pedido_id) {
