8f0695c57c80aae621b718958d58eeb7810fd289
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-04-12 12:46:54 -0500

    cargar

--- resources/views/pedidos/status/poratender.blade.php
+++ resources/views/pedidos/status/poratender.blade.php
@@ -63,7 +63,7 @@
 
 @endsection
 
-@section('css')
+@push('css')
     <style>
     .color-recuperado-abandono
     {
@@ -71,7 +71,7 @@
     color:blue !important;
     }
     </style>
-@stop
+@endpush
 
 @section('js')
   <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
