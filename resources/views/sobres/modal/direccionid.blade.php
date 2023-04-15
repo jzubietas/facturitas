31b21bb3350cb96d03b1b236e69ab33542990ec5
jzubietas <32494930+jzubietas@users.noreply.github.com>
2023-04-15 08:53:16 -0500

    cargar

--- resources/views/sobres/modal/direccionid.blade.php
+++ resources/views/sobres/modal/direccionid.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog  modal-lg">
         <div class="modal-content">
             <div class="modal-header bg-success">
-                <h5 class="modal-title" id="exampleModalLabel">Dirección de envío para cliente </h5>
+                <h5 class="modal-title" id="exampleModalLabel">Dirección de envío para cliente: </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
