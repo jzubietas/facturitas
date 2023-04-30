a04f4ee72a2ddb3b75009aca0ced933371f7fe1c
jzubietas <jhonathanisaizubietasantos@gmail.com>
2023-04-30 18:17:15 -0500

    c

--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,7 @@ Route::middleware(['auth:sanctum', 'verified', 'auth.redirect.is_disabled'])->gr
     Route::any('dashboard.viewMetaTable.General', [DashboardController::class, 'viewMetaTableGeneral'])->name('dashboard.viewMetaTable.General');
     Route::any('dashboard.viewMetaTable', [DashboardController::class, 'viewMetaTable'])->name('dashboard.viewMetaTable');
     Route::any('dashboard.viewMetaTable_G2', [DashboardController::class, 'viewMetaTableG2'])->name('dashboard.viewMetaTable_G2');
+    Route::any('dashboard.viewMetaTable_G3', [DashboardController::class, 'viewMetaTableG3'])->name('dashboard.viewMetaTable_G3');
     Route::any('dashboard.viewMetaTable_G17', [DashboardController::class, 'viewMetaTableG17'])->name('dashboard.viewMetaTable_G17');
     Route::any('dashboard.viewMetaTable.Recurrente.Activo', [DashboardController::class, 'viewMetaTableRecurrenteActivo'])->name('dashboard.viewMetaTable.Recurrente.Activo');
 
