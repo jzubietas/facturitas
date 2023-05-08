2e84b2cd6004395263750d3d3b343dab1f763ef2
jzubietas <jhonathanisaizubietasantos@gmail.com>
2023-05-08 13:00:02 -0500

    c

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2502,7 +2502,7 @@ class DashboardController extends Controller
             }
 
             $html .= '<tr>';
-                $html .= '<td>';
+                $html .= '<td colspan="3">';
                     $html .= '<span class="px-4 pt-1 pb-1 ' . (($object_totales['pedidos_dia'] == 0) ? 'bg-red' : 'bg-white') . '"
                         style="height: 30px !important; color: black !important;">
                         TOTAL DE PEDIDOS DEL DIA: ' . $object_totales['pedidos_dia'] . ' </span>';
