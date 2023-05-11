bc32ec7e5f24d73c94782dfc37eae8ebd7b64a9d
jzubietas <jhonathanisaizubietasantos@gmail.com>
2023-05-11 15:25:55 -0500

    c

--- resources/views/dashboard/partials/vista_presentacion.blade.php
+++ resources/views/dashboard/partials/vista_presentacion.blade.php
@@ -896,7 +896,7 @@ text-shadow: 2px 2px 0 #242120, 2px -2px 0 #242120, -2px 2px 0 #242120, -2px -2p
                 cargaNueva17(17);
                 cargaNueva17(37);
 
-                cargaNuevaCalendario(17);
+                //cargaNuevaCalendario(17);
 
                 cargaNueva99(99);
 
