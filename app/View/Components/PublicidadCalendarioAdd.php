635ca9f1aed7be97716c73e47149c179283f21c4
jzubietas <jhonathanisaizubietasantos@gmail.com>
2023-05-12 18:33:50 -0500

    c

--- app/View/Components/PublicidadCalendarioAdd.php
+++ app/View/Components/PublicidadCalendarioAdd.php
@@ -27,6 +27,12 @@ class PublicidadCalendarioAdd extends Component
         //cargar asistente publicicad
         $publicidad=User::query()->where('rol',User::ROL_ASISTENTE_PUBLICIDAD)->activo()
             ->whereIn('id',[72,95])
+            ->select([
+                \DB::raw("(case when id=72 then 'TOTAL PUBLICIDAD'
+                                when id=95 then 'TOTAL DANTE'
+                                  end) as name"),
+                'id'
+            ])
             ->pluck('name','id');
 
         return view('components.publicidad-calendario-add',compact('publicidad'));
