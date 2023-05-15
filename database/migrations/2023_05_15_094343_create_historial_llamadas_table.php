ab1d74fb26324c5162be4278f1aceaa7165a5027
jzubietas <jhonathanisaizubietasantos@gmail.com>
2023-05-15 09:46:11 -0500

    c

---
+++ database/migrations/2023_05_15_094343_create_historial_llamadas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistorialLlamadasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historial_llamadas', function (Blueprint $table) {
+            $table->id();
+            $table->string('celular')->nullable()->default('');
+            $table->integer('user_registro')->default(0);
+            $table->date('subido')->nullable()->default(Carbon::now());
+            $table->timestamps();
+            $table->integer('estado')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historial_llamadas');
+    }
+}
